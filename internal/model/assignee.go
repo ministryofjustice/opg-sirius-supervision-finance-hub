684bf1cea0461334a607a64e466e665f9fe0c9eb
nicholassully <nicholas.sully@digital.justice.gov.uk>
2024-01-24 10:49:07 +0000

    pfs-3 adding header and footer #minor

---
+++ internal/model/assignee.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"strconv"
+	"strings"
+)
+
+type Assignee struct {
+	Id          int      `json:"id"`
+	Name        string   `json:"displayName"`
+	PhoneNumber string   `json:"phoneNumber"`
+	Deleted     bool     `json:"deleted"`
+	Email       string   `json:"email"`
+	Firstname   string   `json:"firstname"`
+	Surname     string   `json:"surname"`
+	Roles       []string `json:"roles"`
+	Locked      bool     `json:"locked"`
+	Suspended   bool     `json:"suspended"`
+}
+
+func (m Assignee) IsSelected(selectedAssignees []string) bool {
+	for _, a := range selectedAssignees {
+		id, _ := strconv.Atoi(a)
+		if m.Id == id {
+			return true
+		}
+	}
+	return false
+}
+
+func (m Assignee) GetRoles() string {
+	return strings.Join(m.Roles, ",")
+}
--- internal/server/finance_vars.go
+++ internal/server/finance_vars.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"github.com/opg-sirius-finance-hub/internal/model"
 	"github.com/opg-sirius-finance-hub/internal/sirius"
 	"net/http"
 )
@@ -8,6 +9,7 @@ import (
 type FinanceVars struct {
 	Path            string
 	XSRFToken       string
+	MyDetails       model.Assignee
 	SuccessMessage  string
 	Errors          sirius.ValidationErrors
 	EnvironmentVars EnvironmentVars
@@ -18,14 +20,21 @@ type Tab struct {
 }
 
 type FinanceVarsClient interface {
+	GetCurrentUserDetails(sirius.Context) (model.Assignee, error)
 }
 
 func NewFinanceVars(client FinanceVarsClient, r *http.Request, envVars EnvironmentVars) (*FinanceVars, error) {
 	ctx := getContext(r)
 
+	myDetails, err := client.GetCurrentUserDetails(ctx)
+	if err != nil {
+		return nil, err
+	}
+
 	vars := FinanceVars{
 		Path:            r.URL.Path,
 		XSRFToken:       ctx.XSRFToken,
+		MyDetails:       myDetails,
 		EnvironmentVars: envVars,
 	}
 
