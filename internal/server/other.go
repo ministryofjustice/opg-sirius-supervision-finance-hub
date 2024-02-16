c8d31df88dec981bf7c0b35a9c64c0bef4f5c14c
samuel.joseph <denialanderror@googlemail.com>
2024-02-16 10:02:14 +0000

    pfs-23: Fixed tests

--- internal/server/other.go
+++ internal/server/other.go
@@ -18,7 +18,13 @@ type OtherHandler struct {
 func (h OtherHandler) render(v AppVars, w http.ResponseWriter, r *http.Request) error {
 	var data OtherTab
 	data.AppVars = v
-	data.HoldingString = "I am a random number: " + strconv.Itoa(rand.Int())
+
+	// example of how to change the data based on how it is being fetched
+	if isHxRequest(r) {
+		data.HoldingString = "I am a dynamic random number fetched without reloading the page: " + strconv.Itoa(rand.Int())
+	} else {
+		data.HoldingString = "I have been rendered statically on initial page load"
+	}
 
 	h.Data = data
 	return h.execute(w, r)
