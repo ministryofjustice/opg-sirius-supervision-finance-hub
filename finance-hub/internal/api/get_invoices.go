feb9f9cc4bfa74718f958cb43bd82ed080473ab5
samuel.joseph <denialanderror@googlemail.com>
2024-03-05 09:14:08 +0000

    pfs-47: POC for one Go module containing two independent services and some shared data/structs

---
+++ finance-hub/internal/api/get_invoices.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/opg-sirius-finance-hub/shared"
+	"net/http"
+)
+
+func (c *ApiClient) GetInvoices(ctx Context, clientId int) (shared.Invoices, error) {
+	var invoices shared.Invoices
+
+	url := fmt.Sprintf("/api/v1/clients/%d/invoices", clientId)
+
+	req, err := c.newSiriusRequest(ctx, http.MethodGet, url, nil)
+
+	if err != nil {
+		return invoices, err
+	}
+
+	resp, err := c.http.Do(req)
+
+	if err != nil {
+		c.logger.Request(req, err)
+		return invoices, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusUnauthorized {
+		c.logger.Request(req, err)
+		return invoices, ErrUnauthorized
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		c.logger.Request(req, err)
+		return invoices, newStatusError(resp)
+	}
+
+	if err = json.NewDecoder(resp.Body).Decode(&invoices); err != nil {
+		c.logger.Request(req, err)
+		return invoices, err
+	}
+
+	return invoices, err
+}
