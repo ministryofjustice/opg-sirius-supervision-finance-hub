94d73ea162c8dd23df186078188afa0147f627e8
nicholassully <nicholas.sully@digital.justice.gov.uk>
2024-03-27 15:05:53 +0000

    pfs-50 added in the ledger and allocations with summing up

--- finance-api/internal/store/queries/finance_client.sql
+++ finance-api/internal/store/queries/finance_client.sql
@@ -3,3 +3,6 @@ SELECT cacheddebtamount, cachedcreditamount, payment_method FROM finance_client 
 
 -- name: GetInvoices :many
 SELECT id, reference, amount, raiseddate, cacheddebtamount FROM invoice WHERE finance_client_id = $1;
+
+-- name: GetLedgerAllocations :many
+select id, amount, allocateddate, status from ledger_allocation where invoice_id = $1;
