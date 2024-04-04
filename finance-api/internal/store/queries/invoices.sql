67cefe68a0ef9b118160b7ac6efceefb946f963f
nicholassully <nicholas.sully@digital.justice.gov.uk>
2024-04-04 15:49:23 +0100

    pfs-51 rebasing

--- finance-api/internal/store/queries/invoices.sql
+++ finance-api/internal/store/queries/invoices.sql
@@ -2,4 +2,4 @@
 SELECT id, reference, amount, raiseddate, cacheddebtamount FROM invoice WHERE finance_client_id = $1 order by raiseddate desc;
 
 -- name: GetLedgerAllocations :many
-select id, amount, allocateddate, status from ledger_allocation where invoice_id = $1;
+select id, amount, datetime, allocateddate, status from ledger_allocation where invoice_id = $1 order by id desc;
