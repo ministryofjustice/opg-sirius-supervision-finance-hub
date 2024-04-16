7017af2401d2f5985f51f5ee5ee99ac010be8bef
nicholassully <nicholas.sully@digital.justice.gov.uk>
2024-04-16 07:08:17 +0100

    pfs-51 remvoing drop columns

--- migrations/20240410093925_invoice_created_fields.sql
+++ migrations/20240410093925_invoice_created_fields.sql
@@ -3,9 +3,5 @@ ALTER TABLE invoice ADD COLUMN createddate date;
 ALTER TABLE invoice ADD COLUMN createdby_id integer;
 
 -- +goose Down
-ALTER TABLE invoice DROP COLUMN person_id;
-ALTER TABLE invoice DROP COLUMN supervisionlevel;
-ALTER TABLE invoice DROP COLUMN confirmeddate;
-ALTER TABLE invoice DROP COLUMN batchnumber;
 ALTER TABLE invoice DROP COLUMN createddate;
 ALTER TABLE invoice DROP COLUMN createdby_id;
