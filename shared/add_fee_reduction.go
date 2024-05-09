6a70b8b974e773c88f01d79a04b547dbedd39266
nicholassully <nicholas.sully@digital.justice.gov.uk>
2024-05-09 15:49:21 +0100

    pfs-21 removed null check on date as val was not working and made sure no ledgers are created for a whole minimal invoice

--- shared/add_fee_reduction.go
+++ shared/add_fee_reduction.go
@@ -4,6 +4,6 @@ type AddFeeReduction struct {
 	FeeType       string `json:"feeType" validate:"required"`
 	StartYear     string `json:"startYear" validate:"required"`
 	LengthOfAward int    `json:"lengthOfAward" validate:"required,gte=1,lte=3"`
-	DateReceived  Date   `json:"dateReceived" validate:"required,date-in-the-past"`
+	DateReceived  Date   `json:"dateReceived" validate:"date-in-the-past"`
 	Notes         string `json:"notes" validate:"required,thousand-character-limit"`
 }
