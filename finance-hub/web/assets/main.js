3d4399bd00aac64f64c98cd3127a8885ceadd692
samuel.joseph <denialanderror@googlemail.com>
2024-05-14 09:32:05 +0100

    pfs: Trialing hyperscript to tidy up some JS interactions

--- finance-hub/web/assets/main.js
+++ finance-hub/web/assets/main.js
@@ -1,13 +1,16 @@
 import {initAll} from 'govuk-frontend'
 import "govuk-frontend/dist/govuk/all.mjs";
 import "opg-sirius-header/sirius-header.js";
+import _hyperscript from 'hyperscript.org';
+
+_hyperscript.browserInit();
 
 document.body.className += ' js-enabled' + ('noModule' in HTMLScriptElement.prototype ? ' govuk-frontend-supported' : '');
 initAll();
 
 window.htmx = require('htmx.org');
 
-htmx.logAll();
+// htmx.logAll();
 
 // some events will need to occur before the new content is loaded, so register them here on the document itself
 document.body.addEventListener('htmx:beforeOnLoad', function (evt) {
@@ -16,11 +19,6 @@ document.body.addEventListener('htmx:beforeOnLoad', function (evt) {
         evt.detail.shouldSwap = true;
         evt.detail.isError = false;
     }
-
-    // clear the previous validation messages before load so the new ones can be swapped in
-    document.querySelectorAll(".govuk-error-message").forEach((element) => {
-        element.remove();
-    });
 });
 
 // adding event listeners inside the onLoad function will ensure they are re-added to partial content when loaded back in
@@ -40,21 +38,4 @@ htmx.onLoad(content => {
     htmx.findAll(".moj-banner--success").forEach((element) => {
         element.addEventListener("click", () => htmx.addClass(htmx.find(".moj-banner--success"), "hide"));
     });
-
-    // validation errors are loaded in as a partial, with oob-swaps for the field error messages,
-    // but classes need to be applied to each form group that appears in the summary
-    const errorSummary = htmx.find("#error-summary");
-    if (errorSummary) {
-        const errors = [];
-        errorSummary.querySelectorAll(".govuk-link").forEach((element) => {
-            errors.push(element.getAttribute("href"));
-        });
-        htmx.findAll(".govuk-form-group").forEach((element) => {
-            if (errors.includes(`#${element.id}`)) {
-                element.classList.add("govuk-form-group--error");
-            } else {
-                element.classList.remove("govuk-form-group--error");
-            }
-        })
-    }
 });
