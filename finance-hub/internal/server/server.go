aa1f7be3e7fb60fd14e6ec738f1f7761fcf3de50
Sam Joseph <denialanderror@users.noreply.github.com>
2024-05-15 15:08:21 +0100

    PFS: HTMX validation (#56)
    * pfs: Using oob swaps, partials, and JS to swap out only the error summary and messages during validation
    
    * pfs: Disabling Cypress test until PFS-29

--- finance-hub/internal/server/server.go
+++ finance-hub/internal/server/server.go
@@ -43,9 +43,9 @@ func New(logger *zap.SugaredLogger, client ApiClient, templates map[string]*temp
 	mux.Handle("GET /clients/{id}/fee-reductions", wrap(&FeeReductionsHandler{&route{client: client, tmpl: templates["fee-reductions.gotmpl"], partial: "fee-reductions"}}))
 	mux.Handle("GET /clients/{id}/pending-invoice-adjustments", wrap(&PendingInvoiceAdjustmentsHandler{&route{client: client, tmpl: templates["pending-invoice-adjustments.gotmpl"], partial: "pending-invoice-adjustments"}}))
 	mux.Handle("GET /clients/{id}/invoices/{invoiceId}/ledger-entries", wrap(&UpdateInvoiceHandler{&route{client: client, tmpl: templates["update-invoice.gotmpl"], partial: "update-invoice"}}))
-	mux.Handle("POST /clients/{id}/invoices/{invoiceId}/ledger-entries", wrap(&SubmitInvoiceHandler{&route{client: client, tmpl: templates["update-invoice.gotmpl"], partial: "update-invoice"}}))
+	mux.Handle("POST /clients/{id}/invoices/{invoiceId}/ledger-entries", wrap(&SubmitInvoiceHandler{&route{client: client, tmpl: templates["update-invoice.gotmpl"], partial: "error-summary"}}))
 	mux.Handle("GET /clients/{id}/fee-reductions/add", wrap(&UpdateFeeReductionHandler{&route{client: client, tmpl: templates["add-fee-reduction.gotmpl"], partial: "add-fee-reduction"}}))
-	mux.Handle("POST /clients/{id}/fee-reductions/add", wrap(&SubmitFeeReductionsHandler{&route{client: client, tmpl: templates["add-fee-reduction.gotmpl"], partial: "add-fee-reduction"}}))
+	mux.Handle("POST /clients/{id}/fee-reductions/add", wrap(&SubmitFeeReductionsHandler{&route{client: client, tmpl: templates["add-fee-reduction.gotmpl"], partial: "error-summary"}}))
 
 	mux.Handle("/health-check", healthCheck())
 
--- finance-hub/web/assets/main.js
+++ finance-hub/web/assets/main.js
@@ -9,6 +9,20 @@ window.htmx = require('htmx.org');
 
 htmx.logAll();
 
+// some events will need to occur before the new content is loaded, so register them here on the document itself
+document.body.addEventListener('htmx:beforeOnLoad', function (evt) {
+    // htmx by default doesn't swap on error. Status code 422 used to distinguish bad requests from validation errors.
+    if (evt.detail.xhr.status === 422) {
+        evt.detail.shouldSwap = true;
+        evt.detail.isError = false;
+    }
+
+    // clear the previous validation messages before load so the new ones can be swapped in
+    document.querySelectorAll(".govuk-error-message").forEach((element) => {
+        element.remove();
+    });
+});
+
 // adding event listeners inside the onLoad function will ensure they are re-added to partial content when loaded back in
 htmx.onLoad(content => {
     htmx.findAll(content, ".summary").forEach((element => {
@@ -26,12 +40,21 @@ htmx.onLoad(content => {
     htmx.findAll(".moj-banner--success").forEach((element) => {
         element.addEventListener("click", () => htmx.addClass(htmx.find(".moj-banner--success"), "hide"));
     });
-});
 
-// htmx by default doesn't swap on error. Status code 422 used to distinguish bad requests from validation errors.
-document.body.addEventListener('htmx:beforeOnLoad', function (evt) {
-    if (evt.detail.xhr.status === 422) {
-        evt.detail.shouldSwap = true;
-        evt.detail.isError = false;
+    // validation errors are loaded in as a partial, with oob-swaps for the field error messages,
+    // but classes need to be applied to each form group that appears in the summary
+    const errorSummary = htmx.find("#error-summary");
+    if (errorSummary) {
+        const errors = [];
+        errorSummary.querySelectorAll(".govuk-link").forEach((element) => {
+            errors.push(element.getAttribute("href"));
+        });
+        htmx.findAll(".govuk-form-group").forEach((element) => {
+            if (errors.includes(`#${element.id}`)) {
+                element.classList.add("govuk-form-group--error");
+            } else {
+                element.classList.remove("govuk-form-group--error");
+            }
+        })
     }
 });
