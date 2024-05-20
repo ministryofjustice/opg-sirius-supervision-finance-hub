beb6a52b0098d0d9bac086367b2006d29e2663c8
nicholassully <nicholas.sully@digital.justice.gov.uk>
2024-05-20 15:22:39 +0100

    pfs-22 feedback on last pr

--- finance-api/internal/service/cancel_fee_reduction.go
+++ finance-api/internal/service/cancel_fee_reduction.go
@@ -2,31 +2,13 @@ package service
 
 import (
 	"context"
-	"log"
 )
 
 func (s *Service) CancelFeeReduction(id int) error {
 	ctx := context.Background()
 
-	tx, err := s.TX.Begin(ctx)
-	if err != nil {
-		return err
-	}
+	_, err := s.Store.CancelFeeReduction(ctx, int32(id))
 
-	defer func() {
-		if err := tx.Rollback(ctx); err != nil {
-			log.Println("Error rolling back cancel fee reduction transaction:", err)
-		}
-	}()
-
-	transaction := s.Store.WithTx(tx)
-
-	_, err = transaction.CancelFeeReduction(ctx, int32(id))
-	if err != nil {
-		return err
-	}
-
-	err = tx.Commit(ctx)
 	if err != nil {
 		return err
 	}
