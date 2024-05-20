545f46d2932e7bbc52d31864922fd5e826ea78f5
samuel.joseph <denialanderror@googlemail.com>
2024-05-20 13:42:30 +0100

    pfs: Adds Cypress config to provide typing and IDE support

---
+++ cypress/cypress.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+    fixturesFolder: false,
+    e2e: {
+        setupNodeEvents(on, config) {},
+        baseUrl: 'http://localhost:8888',
+        supportFile: false,
+        specPattern: "e2e/**/*.cy.{js,ts}"
+    },
+})
