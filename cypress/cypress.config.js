8eb0fc92a07fcb392314199fc2c90eb24b5bfd83
samuel.joseph <denialanderror@googlemail.com>
2024-05-22 09:54:39 +0100

    pfs-29: Fix cypress grep

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -3,9 +3,11 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
     fixturesFolder: false,
     e2e: {
-        setupNodeEvents(on, config) {},
+        setupNodeEvents(on, config) {
+            require('@cypress/grep/src/plugin')(config);
+            return config;
+        },
         baseUrl: 'http://localhost:8888',
-        supportFile: false,
         specPattern: "e2e/**/*.cy.{js,ts}",
         screenshotsFolder: "./screenshots"
     },
