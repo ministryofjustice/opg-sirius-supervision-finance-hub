64cd03eb35a8e977a5efa9aba49304de308d88f3
Sam Joseph <denialanderror@googlemail.com>
2024-05-30 18:02:47 +0100

    pfs-24: Refactors invoices table BE to only fetch from the DB 3 times rather than 2 times for every invoice

--- finance-api/internal/store/queries/invoices.sql
+++ finance-api/internal/store/queries/invoices.sql
@@ -1,16 +1,29 @@
 SET SEARCH_PATH TO supervision_finance;
 
 -- name: GetInvoices :many
-SELECT i.id, i.reference, i.amount, i.raiseddate, i.cacheddebtamount
+SELECT i.id, i.reference, i.amount, i.raiseddate
 FROM invoice i
-         inner join finance_client fc on fc.id = i.finance_client_id
-where fc.client_id = $1
-order by i.raiseddate desc;
+         INNER JOIN finance_client fc ON fc.id = i.finance_client_id
+WHERE fc.client_id = $1
+ORDER BY i.raiseddate DESC;
+
+-- name: GetLedgerAllocations :many
+SELECT la.invoice_id, la.id, la.amount, la.datetime, l.bankdate, l.type, la.status
+FROM ledger_allocation la
+         INNER JOIN ledger l ON la.ledger_id = l.id
+WHERE la.invoice_id = ANY($1::int[])
+ORDER BY la.id DESC;
+
+-- name: GetSupervisionLevels :many
+SELECT invoice_id, supervisionlevel, fromdate, todate, amount
+FROM invoice_fee_range
+WHERE invoice_id = ANY($1::int[])
+ORDER BY todate DESC;
 
 -- name: GetInvoiceBalance :one
 SELECT i.amount initial, i.amount - COALESCE(SUM(la.amount), 0) outstanding, i.feetype
 FROM invoice i
-         LEFT JOIN ledger_allocation la on i.id = la.invoice_id
+         LEFT JOIN ledger_allocation la ON i.id = la.invoice_id
     AND la.status <> 'PENDING'
 WHERE i.id = $1
 group by i.amount, i.feetype;
@@ -29,17 +42,15 @@ where invoice_id = $1
 order by todate desc;
 
 -- name: AddFeeReductionToInvoices :many
-WITH filtered_invoices AS (
-    SELECT i.id AS invoice_id, fr.id AS fee_reduction_id
-    FROM invoice i
-             JOIN fee_reduction fr
-                  ON i.finance_client_id = fr.finance_client_id
-    WHERE i.raiseddate >= (fr.datereceived - interval '6 months')
-      AND i.raiseddate BETWEEN fr.startdate AND fr.enddate
-      AND fr.id = $1
-)
+WITH filtered_invoices AS (SELECT i.id AS invoice_id, fr.id AS fee_reduction_id
+                           FROM invoice i
+                                    JOIN fee_reduction fr
+                                         ON i.finance_client_id = fr.finance_client_id
+                           WHERE i.raiseddate >= (fr.datereceived - INTERVAL '6 months')
+                             AND i.raiseddate BETWEEN fr.startdate AND fr.enddate
+                             AND fr.id = $1)
 UPDATE invoice i
 SET fee_reduction_id = fi.fee_reduction_id
 FROM filtered_invoices fi
 WHERE i.id = fi.invoice_id
-returning i.*;
+RETURNING i.*;
