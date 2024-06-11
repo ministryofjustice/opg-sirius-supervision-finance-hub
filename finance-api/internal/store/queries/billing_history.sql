e7118fc1b27525f58977883f3e62e7b278179257
Sam Joseph <denialanderror@googlemail.com>
2024-06-11 14:27:30 +0100

    pfs-86: Switch to using confirmeddate as the generated/applied date

--- finance-api/internal/store/queries/billing_history.sql
+++ finance-api/internal/store/queries/billing_history.sql
@@ -1,16 +1,16 @@
 -- name: GetGeneratedInvoices :many
-SELECT i.id invoice_id, reference, feetype, amount, createddate, createdby_id
+SELECT i.id invoice_id, reference, feetype, amount, confirmeddate, createdby_id
 FROM invoice i
          JOIN finance_client fc ON fc.id = i.finance_client_id
 WHERE fc.client_id = $1
-ORDER BY createddate DESC;
+ORDER BY confirmeddate DESC;
 
 -- name: GetAppliedLedgerAllocations :many
-SELECT i.id invoice_id, l.id ledger_id, i.reference, l.type, la.amount, l.notes, l.createddate, l.createdby_id
+SELECT i.id invoice_id, l.id ledger_id, i.reference, l.type, la.amount, l.notes, l.confirmeddate, l.createdby_id
 FROM ledger_allocation la
          JOIN ledger l ON l.id = la.ledger_id
          JOIN invoice i ON i.id = la.invoice_id
          JOIN finance_client fc ON fc.id = i.finance_client_id
 WHERE fc.client_id = $1
 AND l.status IN ('APPROVED', 'CONFIRMED')
-ORDER BY l.createddate DESC;
+ORDER BY l.confirmeddate DESC;
