b8ead8eac8e2406c24efe62120a440fa28880e7a
Sam Joseph <denialanderror@googlemail.com>
2024-06-14 13:40:29 +0100

    pfs-17: Fixes query calc that grouped by incorrectly and missed duplicate values

--- finance-api/internal/service/finance_clients_test.go
+++ finance-api/internal/service/finance_clients_test.go
@@ -14,8 +14,11 @@ func (suite *IntegrationSuite) TestService_GetAccountInformation() {
 		"INSERT INTO finance_client VALUES (1, 1, 'sop123', 'DEMANDED', NULL)",
 		"INSERT INTO finance_client VALUES (3, 3, 'sop123', 'DEMANDED', NULL)",
 		"INSERT INTO invoice VALUES (1, 1, 1, 'S2', 'S203531/19', '2019-04-01', '2020-03-31', 32000, NULL, '2020-03-20',1, '2020-03-16', 10, NULL, 0, '2019-06-06', 99);",
-		"INSERT INTO ledger VALUES (1, 'random1223', '2022-04-11T08:36:40+00:00', '', 12000, '', 'CARD PAYMENT', 'APPROVED', 1, NULL, NULL, '11/04/2022', '12/04/2022', 1254, '', '', 1, '05/05/2022', 65);",
+		"INSERT INTO invoice VALUES (2, 1, 1, 'S2', 'S203532/20', '2020-04-01', '2021-03-31', 32000, NULL, '2020-03-20',1, '2020-03-16', 10, NULL, 0, '2019-06-06', 99);",
+		"INSERT INTO ledger VALUES (1, 'random123', '2022-04-11T08:36:40+00:00', '', 12000, '', 'CARD PAYMENT', 'APPROVED', 1, NULL, NULL, '11/04/2022', '12/04/2022', 1254, '', '', 1, '05/05/2022', 65);",
+		"INSERT INTO ledger VALUES (2, 'random456', '2022-04-11T08:36:40+00:00', '', 12000, '', 'CARD PAYMENT', 'APPROVED', 1, NULL, NULL, '11/04/2022', '12/04/2022', 1254, '', '', 1, '05/05/2022', 65);",
 		"INSERT INTO ledger_allocation VALUES (1, 1, 1, '2022-04-11T08:36:40+00:00', 12000, 'APPROVED', NULL, 'Notes here', '2022-04-11', NULL);",
+		"INSERT INTO ledger_allocation VALUES (2, 2, 2, '2022-04-11T08:36:40+00:00', 12000, 'APPROVED', NULL, 'Notes here', '2022-04-11', NULL);",
 	)
 
 	Store := store.New(conn)
@@ -29,7 +32,7 @@ func (suite *IntegrationSuite) TestService_GetAccountInformation() {
 			name: "returns account information when clientId matches clientId in finance_client table",
 			id:   1,
 			want: &shared.AccountInformation{
-				OutstandingBalance: 20000,
+				OutstandingBalance: 40000,
 				CreditBalance:      0,
 				PaymentMethod:      "DEMANDED",
 			},
--- finance-api/internal/store/finance_client.sql.go
+++ finance-api/internal/store/finance_client.sql.go
@@ -15,12 +15,11 @@ FROM finance_client fc
          LEFT JOIN invoice i ON fc.id = i.finance_client_id
          LEFT JOIN ledger_allocation la ON i.id = la.invoice_id AND la.status <> 'PENDING'
 WHERE fc.client_id = $1
-GROUP BY fc.client_id, i.amount)
-SELECT SUM(balances.total) - SUM(balances.paid) outstanding, 0 credit, fc.payment_method
+GROUP BY fc.client_id)
+SELECT balances.total - balances.paid outstanding, 0 credit, fc.payment_method
 FROM finance_client fc
 JOIN balances ON fc.client_id = balances.client_id
 WHERE fc.client_id = $1
-GROUP BY fc.payment_method
 `
 
 type GetAccountInformationRow struct {
