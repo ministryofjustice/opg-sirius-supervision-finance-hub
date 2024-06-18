f6c8e835222315e633ba9f6ade07650284a40d98
Sam Joseph <denialanderror@googlemail.com>
2024-06-18 14:26:49 +0100

    pfs-17: Accept APPROVED and ALLOCATED until a decision is made on the best approach

--- finance-api/internal/store/finance_client.sql.go
+++ finance-api/internal/store/finance_client.sql.go
@@ -13,7 +13,7 @@ const getAccountInformation = `-- name: GetAccountInformation :one
 WITH balances AS (SELECT i.id, fc.client_id, i.amount, COALESCE(SUM(la.amount), 0) paid
 FROM finance_client fc
          LEFT JOIN invoice i ON fc.id = i.finance_client_id
-         LEFT JOIN ledger_allocation la ON i.id = la.invoice_id AND la.status = 'ALLOCATED'
+         LEFT JOIN ledger_allocation la ON i.id = la.invoice_id AND la.status IN ('ALLOCATED', 'APPROVED')
 WHERE fc.client_id = $1
 GROUP BY i.id, fc.client_id)
 SELECT COALESCE(SUM(balances.amount), 0) - SUM(balances.paid) outstanding, 0 credit, fc.payment_method
--- finance-api/internal/store/queries/finance_client.sql
+++ finance-api/internal/store/queries/finance_client.sql
@@ -2,7 +2,7 @@
 WITH balances AS (SELECT i.id, fc.client_id, i.amount, COALESCE(SUM(la.amount), 0) paid
 FROM finance_client fc
          LEFT JOIN invoice i ON fc.id = i.finance_client_id
-         LEFT JOIN ledger_allocation la ON i.id = la.invoice_id AND la.status = 'ALLOCATED'
+         LEFT JOIN ledger_allocation la ON i.id = la.invoice_id AND la.status IN ('ALLOCATED', 'APPROVED')
 WHERE fc.client_id = $1
 GROUP BY i.id, fc.client_id)
 SELECT COALESCE(SUM(balances.amount), 0) - SUM(balances.paid) outstanding, 0 credit, fc.payment_method
