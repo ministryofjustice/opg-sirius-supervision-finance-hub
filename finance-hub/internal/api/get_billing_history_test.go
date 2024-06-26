2052fff45fcb637670c5ac802a01801607042f0b
Nicholassully <31247234+Nicholassully@users.noreply.github.com>
2024-06-26 11:31:54 +0100

    pfs-77 frontend ui (#87)
    * pfs-77 frontend ui
    
    * pfs-77 unit tests for billing history
    
    * pfs-77 cypress fixes

---
+++ finance-hub/internal/api/get_billing_history_test.go
@@ -0,0 +1,113 @@
+package api
+
+import (
+	"bytes"
+	"github.com/opg-sirius-finance-hub/shared"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestGetBillingHistoryCanReturn200(t *testing.T) {
+	logger, mockClient := SetUpTest()
+	client, _ := NewApiClient(mockClient, "http://localhost:3000", "", logger)
+
+	json := `
+	[
+	   {
+		  "clientId": 456,
+		  "user": "65",
+		  "date": "2099-11-04T15:04:05+00:00",
+		  "event": {
+			"client_id": "1",
+			"type": "INVOICE_ADJUSTMENT_APPLIED",
+			"adjustment_type": "Write off",
+			"total": 12300,
+			"payment_breakdown": {
+			  "invoice_reference": {
+				"id": 1,
+				"reference": "S203531/19"
+			  },
+			  "amount": 12300
+			},
+			"baseBillingEvent": {
+			  "type": 4
+			}
+		  },
+		  "outstanding_balance": 0
+		}
+	]
+	`
+
+	r := io.NopCloser(bytes.NewReader([]byte(json)))
+
+	GetDoFunc = func(rq *http.Request) (*http.Response, error) {
+		return &http.Response{
+			StatusCode: 200,
+			Body:       r,
+		}, nil
+	}
+
+	expectedResponse := []shared.BillingHistory{
+		{
+			User: "65",
+			Date: shared.Date{Time: time.Date(2099, time.November, 4, 15, 4, 5, 0, time.UTC)},
+			Event: &shared.InvoiceAdjustmentApproved{
+				AdjustmentType: "Write off",
+				ClientId:       "1",
+				PaymentBreakdown: shared.PaymentBreakdown{
+					InvoiceReference: shared.InvoiceEvent{
+						ID:        1,
+						Reference: "S203531/19",
+					},
+					Amount: 12300,
+				},
+				BaseBillingEvent: shared.BaseBillingEvent{Type: 4},
+			},
+			OutstandingBalance: 0,
+		},
+	}
+
+	invoiceList, err := client.GetBillingHistory(getContext(nil), 456)
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, expectedResponse, invoiceList)
+}
+
+func TestGetBillingHistoryCanThrow500Error(t *testing.T) {
+	logger, _ := SetUpTest()
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	}))
+	defer svr.Close()
+
+	client, _ := NewApiClient(http.DefaultClient, svr.URL, svr.URL, logger)
+
+	_, err := client.GetBillingHistory(getContext(nil), 1)
+
+	assert.Equal(t, StatusError{
+		Code:   http.StatusInternalServerError,
+		URL:    svr.URL + "/api/v1/clients/1/billing-history",
+		Method: http.MethodGet,
+	}, err)
+}
+
+func TestGetBillingHistoryUnauthorised(t *testing.T) {
+	logger, _ := SetUpTest()
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusUnauthorized)
+	}))
+	defer svr.Close()
+
+	client, _ := NewApiClient(http.DefaultClient, svr.URL, svr.URL, logger)
+
+	clientList, err := client.GetBillingHistory(getContext(nil), 3)
+
+	var expectedResponse []shared.BillingHistory
+
+	assert.Equal(t, expectedResponse, clientList)
+	assert.Equal(t, ErrUnauthorized, err)
+}
---
+++ finance-hub/internal/server/billing_history_test.go
@@ -0,0 +1,71 @@
+package server
+
+import (
+	"errors"
+	"github.com/opg-sirius-finance-hub/shared"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestBillingHistory(t *testing.T) {
+	data := []shared.BillingHistory{
+		{
+			User: "Someone here",
+			Date: shared.NewDate("01/04/2222"),
+			Event: shared.InvoiceGenerated{
+				ClientId: "456",
+				InvoiceReference: shared.InvoiceEvent{
+					ID:        1,
+					Reference: "A12345678/02",
+				},
+				InvoiceType:      "AD",
+				InvoiceName:      "The name of the invoice",
+				Amount:           65498,
+				BaseBillingEvent: shared.BaseBillingEvent{Type: 1},
+			},
+			OutstandingBalance: 25124,
+		},
+	}
+
+	client := mockApiClient{BillingHistory: data}
+	ro := &mockRoute{client: client}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "", nil)
+	r.SetPathValue("clientId", "456")
+
+	appVars := AppVars{Path: "/path/"}
+
+	sut := BillingHistoryHandler{ro}
+	err := sut.render(appVars, w, r)
+
+	assert.Nil(t, err)
+	assert.True(t, ro.executed)
+
+	expected := &BillingHistoryVars{
+		BillingHistory: data,
+		AppVars:        appVars,
+	}
+
+	assert.Equal(t, expected, ro.data)
+}
+
+func TestBillingHistoryErrors(t *testing.T) {
+	client := mockApiClient{}
+	client.error = errors.New("this has failed")
+	ro := &mockRoute{client: client}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "", nil)
+	r.SetPathValue("clientId", "1")
+
+	appVars := AppVars{Path: "/path/"}
+
+	sut := BillingHistoryHandler{ro}
+	err := sut.render(appVars, w, r)
+
+	assert.Equal(t, "this has failed", err.Error())
+	assert.False(t, ro.executed)
+}
