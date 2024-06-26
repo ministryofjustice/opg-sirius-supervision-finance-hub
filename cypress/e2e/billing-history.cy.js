4c30c561868b0fa8c3ed3435f8f724849b475dbe
nicholassully <nicholas.sully@digital.justice.gov.uk>
2024-06-26 10:49:39 +0100

    pfs-77 cypress fixes

--- cypress/e2e/billing-history.cy.js
+++ cypress/e2e/billing-history.cy.js
@@ -1,7 +1,7 @@
 describe("Billing History Tab", () => {
     it("the feed of the billing history show", () => {
         cy.visit("/clients/1/invoices");
-        cy.get('[data-cy="billing-history"]').click()
+        cy.contains('a', 'Billing History').click();
         cy.get('.moj-timeline').first().contains("Write off applied to");
         cy.get('.govuk-link').first().click();
         cy.url().should('include', "clients/1/invoices");
@@ -9,6 +9,6 @@ describe("Billing History Tab", () => {
 
     it("no history shows correct message", () => {
         cy.visit("/clients/2/billing-history");
-        cy.contains('[data-cy="no-billing-history"]', "No billing history for this client");
+        cy.contains('h2.moj-timeline__title', 'No billing history for this client').should('be.visible');
     });
 });
