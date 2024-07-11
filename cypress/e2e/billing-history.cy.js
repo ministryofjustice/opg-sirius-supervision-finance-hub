1112349420c645627710d97df1a14b98be1eff29
nicholassully <nicholas.sully@digital.justice.gov.uk>
2024-07-11 15:24:43 +0100

    pfs-78 sorting if the date is the same

--- cypress/e2e/billing-history.cy.js
+++ cypress/e2e/billing-history.cy.js
@@ -4,10 +4,10 @@ describe("Billing History Tab", () => {
         cy.contains('a', 'Billing History').click();
         cy.url().should('include', "clients/1/billing-history");
 
-        cy.get('.moj-timeline__title').first().contains("S2 - created for £320");
+        cy.get('.moj-timeline__title').first().contains("S206666/18 invoice created for £320");
         cy.get('.moj-timeline__date').first().contains("Outstanding balance: £320 Credit balance: £0");
-        // cy.get('.govuk-link').first().click();
-        // cy.url().should('include', "clients/1/invoices");
+        cy.get('.govuk-link').first().click();
+        cy.url().should('include', "clients/1/invoices");
     });
 
     it("no history shows correct message", () => {
