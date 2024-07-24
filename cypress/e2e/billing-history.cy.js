15cd4340c3573fc38c1475a0d12964b34d70b9f0
Sam Joseph <denialanderror@googlemail.com>
2024-07-24 11:46:44 +0100

    PFS-18: Customer Credit Balance can be stored and calculated from ledger allocations

--- cypress/e2e/billing-history.cy.js
+++ cypress/e2e/billing-history.cy.js
@@ -1,13 +1,13 @@
 describe("Billing History Tab", () => {
     it("displays the billing history", () => {
         cy.visit("/clients/1/invoices");
-        cy.contains('a', 'Billing History').click();
-        cy.url().should('include', "clients/1/billing-history");
+        cy.contains("a", "Billing History").click();
+        cy.url().should("include", "clients/1/billing-history");
 
-        cy.get('.moj-timeline__title').first().contains("S2 invoice created for £320");
-        cy.get('.moj-timeline__date').first().contains("Outstanding balance: £420 Credit balance: £0");
-        cy.get('.govuk-link').first().click();
-        cy.url().should('include', "clients/1/invoices");
+        cy.get(".moj-timeline__title").first().contains("Pending credit write off of £12 added to AD03531/19");
+        cy.get(".moj-timeline__date").first().contains("Outstanding balance: £520 Credit balance: £0");
+        cy.contains(".govuk-link", "AD03531/19").click();
+        cy.url().should("include", "clients/1/invoices");
     });
 
     it("no history shows correct message", () => {
