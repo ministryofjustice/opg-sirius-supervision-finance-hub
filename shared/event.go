0bd0e1c689fbd60921e1abe3e1bfe9c7a46db765
Sam Joseph <denialanderror@googlemail.com>
2024-08-20 17:01:00 +0100

    pfs-103: Event handler for eventbridge events

---
+++ shared/event.go
@@ -0,0 +1,50 @@
+package shared
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+const (
+	EventSourceSirius             = "opg.supervision.sirius"
+	DetailTypeDebtPositionChanged = "debt-position-changed"
+)
+
+type Event struct {
+	Source       string      `json:"source"`
+	EventBusName string      `json:"eventBusName"`
+	DetailType   string      `json:"detailType"`
+	Detail       interface{} `json:"detail"`
+}
+
+func (e *Event) UnmarshalJSON(data []byte) error {
+	type tmp Event // avoids infinite recursion
+	if err := json.Unmarshal(data, (*tmp)(e)); err != nil {
+		return err
+	}
+
+	var raw struct {
+		Detail json.RawMessage `json:"detail"`
+	}
+
+	if err := json.Unmarshal(data, &raw); err != nil {
+		return err
+	}
+
+	switch e.DetailType {
+	case DetailTypeDebtPositionChanged:
+		var detail DebtPositionChangedEvent
+		if err := json.Unmarshal(raw.Detail, &detail); err != nil {
+			return err
+		}
+		e.Detail = detail
+	default:
+		return fmt.Errorf("unknown detail type: %s", e.DetailType)
+	}
+
+	return nil
+}
+
+type DebtPositionChangedEvent struct {
+	ClientID int `json:"clientId"`
+}
