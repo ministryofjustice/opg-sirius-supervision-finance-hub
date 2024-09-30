974dd348ad501b078944fa69b3b591316dae6845
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2024-09-30 17:30:01 +0100

    PFS-130 Fetch file from S3 & read records

--- finance-api/cmd/api/handle_events.go
+++ finance-api/cmd/api/handle_events.go
@@ -19,8 +19,6 @@ func (s *Server) handleEvents(w http.ResponseWriter, r *http.Request) error {
 		return apierror.BadRequestError("event", "unable to parse event", err)
 	}
 
-	fmt.Println(event.Detail)
-
 	if event.Source == shared.EventSourceSirius && event.DetailType == shared.DetailTypeDebtPositionChanged {
 		if detail, ok := event.Detail.(shared.DebtPositionChangedEvent); ok {
 			err := s.Service.ReapplyCredit(ctx, int32(detail.ClientID))
--- finance-api/internal/service/process_finance_admin_upload.go
+++ finance-api/internal/service/process_finance_admin_upload.go
@@ -2,11 +2,56 @@ package service
 
 import (
 	"context"
+	"encoding/csv"
 	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"os"
 )
 
 func (s *Service) ProcessFinanceAdminUpload(ctx context.Context, bucketName string, key string) error {
-	fmt.Println(bucketName)
-	fmt.Println(key)
+	awsRegion := os.Getenv("AWS_REGION")
+
+	cfg, err := config.LoadDefaultConfig(
+		ctx,
+		config.WithRegion(awsRegion),
+	)
+	if err != nil {
+		return err
+	}
+
+	if iamRole, ok := os.LookupEnv("AWS_IAM_ROLE"); ok {
+		client := sts.NewFromConfig(cfg)
+		cfg.Credentials = stscreds.NewAssumeRoleProvider(client, iamRole)
+	}
+
+	client := s3.NewFromConfig(cfg, func(u *s3.Options) {
+		u.UsePathStyle = true
+		u.Region = awsRegion
+
+		endpoint := os.Getenv("AWS_S3_ENDPOINT")
+		if endpoint != "" {
+			u.BaseEndpoint = &endpoint
+		}
+	})
+
+	output, err := client.GetObject(ctx, &s3.GetObjectInput{
+		Key:    aws.String(key),
+		Bucket: aws.String(bucketName),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	csvReader := csv.NewReader(output.Body)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		return err
+	}
+	fmt.Println(records)
 	return nil
 }
