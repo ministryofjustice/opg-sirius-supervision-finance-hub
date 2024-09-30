3f8548ec4383bb7638b3de80ca1930eed1abadf0
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2024-09-30 15:32:45 +0100

    PFS-130 Fix failing tests

--- finance-api/cmd/api/server_test.go
+++ finance-api/cmd/api/server_test.go
@@ -94,3 +94,7 @@ func (s *mockService) AddInvoiceAdjustment(ctx context.Context, clientId int, in
 	s.lastCalled = "AddInvoiceAdjustment"
 	return s.invoiceReference, s.err
 }
+
+func (s *mockService) ProcessFinanceAdminUpload(ctx context.Context, bucketName string, key string) error {
+	return s.err
+}
