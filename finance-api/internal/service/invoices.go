a5f626c527ba1ebc7f92670eff82fd7704c1f917
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2024-11-13 10:39:43 +0000

    PFS-133 Move translate transaction type to frontend

--- finance-api/internal/service/invoices.go
+++ finance-api/internal/service/invoices.go
@@ -86,19 +86,13 @@ func (ib *invoiceBuilder) addLedgerAllocations(ilas []store.GetLedgerAllocations
 	for _, il := range ilas {
 		metadata := ib.invoices[il.InvoiceID.Int32]
 
-		transactionType := shared.ParseTransactionType(il.Type).String()
-		if transactionType == "" {
-			transactionType = il.Type
-		}
-
 		metadata.invoice.Ledgers = append(
 			metadata.invoice.Ledgers,
 			shared.Ledger{
 				Amount:          int(il.Amount),
 				ReceivedDate:    shared.Date{Time: il.RaisedDate.Time},
-				TransactionType: transactionType,
-				//TransactionType: il.Type,
-				Status: il.Status,
+				TransactionType: il.Type,
+				Status:          il.Status,
 			})
 
 		if il.Type == "WRITE OFF REVERSAL" {
