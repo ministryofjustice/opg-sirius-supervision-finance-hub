da7c837e378da5607d6601e884719f14ccac3e20
Sam Joseph <denialanderror@googlemail.com>
2024-11-27 16:49:21 +0000

    pfs-152: Updates to include legacy types

--- migrations/20241126103709_accounting_strings_code_change.sql
+++ migrations/20241126103709_accounting_strings_code_change.sql
@@ -21,6 +21,8 @@ VALUES
 ('BC', 'AD', 'OPG BACS PAYMENT', 1816100000, 'BACS Payment'),
 ('CQ', 'AD', 'CHEQUE PAYMENT', 1816100000, 'Cheque Payment'),
 ('RA', 'AD', 'CREDIT REAPPLY', 1816100000, 'Reapply/Reallocate (money to invoice)'),
+('', 'AD', 'BACS TRANSFER', 1816100000, 'BACS Payment'),
+('', 'AD', 'CARD PAYMENT', 1816100000, 'Card Payment'),
 ('DD', 'GENERAL', 'DIRECT DEBIT PAYMENT', 1816100000, 'Direct Debit Payment'),
 ('OC', 'GENERAL', 'ONLINE CARD PAYMENT', 1816100000, 'Online Card Payment'),
 ('PC', 'GENERAL', 'MOTO CARD PAYMENT', 1816100000, 'MOTO (phone) Card Payment'),
@@ -28,17 +30,21 @@ VALUES
 ('BC', 'GENERAL', 'OPG BACS PAYMENT', 1816100000, 'BACS Payment'),
 ('CQ', 'GENERAL', 'CHEQUE PAYMENT', 1816100000, 'Cheque Payment'),
 ('RA', 'GENERAL', 'CREDIT REAPPLY', 1816100000, 'Reapply/Reallocate (money to invoice)'),
+('', 'GENERAL', 'BACS TRANSFER', 1816100000, 'BACS Transfer'),
+('', 'GENERAL', 'CARD PAYMENT', 1816100000, 'Card Payment'),
 ('DD', 'MINIMAL', 'DIRECT DEBIT PAYMENT', 1816100000, 'Direct Debit Payment'),
 ('OC', 'MINIMAL', 'ONLINE CARD PAYMENT', 1816100000, 'Online Card Payment'),
 ('PC', 'MINIMAL', 'MOTO CARD PAYMENT', 1816100000, 'MOTO (phone) Card Payment'),
 ('BC', 'MINIMAL', 'SUPERVISION BACS PAYMENT', 1816100000, 'BACS Payment'),
 ('BC', 'MINIMAL', 'OPG BACS PAYMENT', 1816100000, 'BACS Payment'),
 ('CQ', 'MINIMAL', 'CHEQUE PAYMENT', 1816100000, 'Cheque Payment'),
-('RA', 'MINIMAL', 'CREDIT REAPPLY', 1816100000, 'Reapply/Reallocate (money to invoice)');
+('RA', 'MINIMAL', 'CREDIT REAPPLY', 1816100000, 'Reapply/Reallocate (money to invoice)'),
+('', 'MINIMAL', 'BACS TRANSFER', 1816100000, 'BACS Payment'),
+('', 'MINIMAL', 'CARD PAYMENT', 1816100000, 'Card Payment');
 
 -- +goose Down
 
-DELETE FROM transaction_type WHERE fee_type IN ('DD', 'OC', 'PC', 'BC', 'CQ');
+DELETE FROM transaction_type WHERE fee_type IN ('DD', 'OC', 'PC', 'BC', 'CQ', '');
 
 UPDATE transaction_type
 SET account_code = CASE
