e2ad9a982fe4f650c6bb3feac95a50424b387e27
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2025-01-13 13:39:03 +0000

    PFS-198 Add fromdate & todate to query

--- finance-api/internal/reports/generate_report.go
+++ finance-api/internal/reports/generate_report.go
@@ -30,7 +30,10 @@ func (c *Client) GenerateAndUploadReport(ctx context.Context, reportRequest shar
 		case shared.ReportAccountTypeAgedDebtByCustomer:
 			query = &db.AgedDebtByCustomer{}
 		case shared.ReportAccountTypeARPaidInvoiceReport:
-			query = &db.PaidInvoices{}
+			query = &db.PaidInvoices{
+				FromDate: reportRequest.FromDateField,
+				ToDate:   reportRequest.ToDateField,
+			}
 		default:
 			return fmt.Errorf("unknown query")
 		}
