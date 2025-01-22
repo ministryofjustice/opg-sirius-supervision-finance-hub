7c598bc1a556fe31c91ed73ef7992f00b979de00
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2025-01-22 11:38:22 +0000

    PFS-197 Fix unit test

--- finance-api/internal/reports/generate_report_test.go
+++ finance-api/internal/reports/generate_report_test.go
@@ -139,7 +139,7 @@ func TestCreateDownloadNotifyPayload(t *testing.T) {
 	uid, _ := downloadRequest.Encode()
 	requestedDate, _ := time.Parse("2006-01-02 15:04:05", "2024-01-01 13:37:00")
 	_ = os.Setenv("SIRIUS_PUBLIC_URL", "www.sirius.com")
-	_ = os.Setenv("PREFIX", "/finance")
+	_ = os.Setenv("FINANCE_ADMIN_PREFIX", "/finance")
 
 	want := notify.Payload{
 		EmailAddress: emailAddress,
