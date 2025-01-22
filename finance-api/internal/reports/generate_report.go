33ed1d294ded1b8bd76307cb8ab7ffa398c4143c
Sam Joseph <denialanderror@googlemail.com>
2025-01-22 12:26:05 +0000

    pfs-201: updates report types

--- finance-api/internal/reports/generate_report.go
+++ finance-api/internal/reports/generate_report.go
@@ -34,7 +34,7 @@ func (c *Client) GenerateAndUploadReport(ctx context.Context, reportRequest shar
 				FromDate: reportRequest.FromDateField,
 				ToDate:   reportRequest.ToDateField,
 			}
-		case shared.ReportAccountTypeCustomerCredit:
+		case shared.ReportAccountTypeUnappliedReceipts:
 			query = &db.CustomerCredit{}
 		default:
 			return fmt.Errorf("unknown query")
