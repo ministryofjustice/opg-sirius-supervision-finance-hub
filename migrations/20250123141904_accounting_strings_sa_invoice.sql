e84bfd253c9c234318949c3398cf5bcb60bc0f3a
Sam Joseph <denialanderror@googlemail.com>
2025-01-27 14:39:02 +0000

    pfs-206: Removes SA invoice migration

--- migrations/20250123141904_accounting_strings_sa_invoice.sql
+++
@@ -1,8 +0,0 @@
--- +goose Up
-INSERT INTO transaction_type (fee_type, supervision_level, ledger_type, account_code, description)
-VALUES
-    ('SA', '', '', 4481102093, 'SA - General invoice (Legacy)');
-
--- +goose Down
-
-DELETE FROM transaction_type WHERE fee_type = 'SA';
