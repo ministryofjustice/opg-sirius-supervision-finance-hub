2c2db8e2d6d877cc81e368725f49208efdc5a5a8
Sam Joseph <denialanderror@googlemail.com>
2025-02-14 16:21:18 +0000

    pfs-237: Adds JWT verification to BE

---
+++ finance-api/internal/auth/jwt.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/ministryofjustice/opg-sirius-supervision-finance-hub/shared"
+)
+
+type Context struct {
+	context.Context
+	User *shared.User
+}
+
+type JWT struct {
+	Secret string
+	Expiry int
+}
+
+type Claims struct {
+	Roles []string `json:"roles"`
+	jwt.RegisteredClaims
+}
+
+func (j JWT) Verify(requestToken string) (*jwt.Token, error) {
+	token, err := jwt.ParseWithClaims(requestToken, &Claims{}, func(token *jwt.Token) (i interface{}, err error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(j.Secret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
