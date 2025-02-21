3f2b0bdf80424687522e5d86215c5bdd3a52b9b8
Sam Joseph <denialanderror@googlemail.com>
2025-02-21 16:42:23 +0000

    pfs-81: Adds role checks to buttons in the UI, along with user switcher function for Cypress/json-server

--- shared/user.go
+++ shared/user.go
@@ -5,3 +5,28 @@ type User struct {
 	DisplayName string   `json:"displayName"`
 	Roles       []string `json:"roles"`
 }
+
+func (u User) IsFinanceUser() bool {
+	return contains(u.Roles, "Finance User")
+}
+
+func (u User) IsFinanceAdmin() bool {
+	return contains(u.Roles, "Finance Admin")
+}
+
+func (u User) IsFinanceManager() bool {
+	return contains(u.Roles, "Finance Manager")
+}
+
+func (u User) IsCorporateFinance() bool {
+	return contains(u.Roles, "Corporate Finance")
+}
+
+func contains(haystack []string, needle string) bool {
+	for _, h := range haystack {
+		if h == needle {
+			return true
+		}
+	}
+	return false
+}
