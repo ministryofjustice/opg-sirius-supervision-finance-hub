01e79a1d63244c3689c2b4c55ad4a305e05bc6b2
Sam Joseph <denialanderror@googlemail.com>
2025-02-24 14:38:07 +0000

    pfs-81: Auth added to backend

--- shared/user.go
+++ shared/user.go
@@ -1,5 +1,13 @@
 package shared
 
+const (
+	RoleFinanceUser      = "Finance User"
+	RoleFinanceManager   = "Finance Manager"
+	RoleFinanceReporting = "Finance Reporting"
+	RoleCorporateFinance = "Corporate Finance"
+	RoleAny              = ""
+)
+
 type User struct {
 	ID          int32    `json:"id"`
 	DisplayName string   `json:"displayName"`
@@ -7,19 +15,26 @@ type User struct {
 }
 
 func (u User) IsFinanceUser() bool {
-	return contains(u.Roles, "Finance User")
-}
-
-func (u User) IsFinanceAdmin() bool {
-	return contains(u.Roles, "Finance Admin")
+	return contains(u.Roles, RoleFinanceUser)
 }
 
 func (u User) IsFinanceManager() bool {
-	return contains(u.Roles, "Finance Manager")
+	return contains(u.Roles, RoleFinanceManager)
+}
+
+func (u User) IsFinanceReporting() bool {
+	return contains(u.Roles, RoleFinanceReporting)
 }
 
 func (u User) IsCorporateFinance() bool {
-	return contains(u.Roles, "Corporate Finance")
+	return contains(u.Roles, RoleCorporateFinance)
+}
+
+func (u User) HasRole(role string) bool {
+	if role == RoleAny {
+		return true
+	}
+	return contains(u.Roles, role)
 }
 
 func contains(haystack []string, needle string) bool {
