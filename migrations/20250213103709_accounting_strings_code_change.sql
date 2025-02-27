481a614c05489a756b9a7283905e8363f38103d9
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2025-02-27 18:11:44 +0000

    PFS-161 Merge accounting code migrations & add guardianship ledger types

--- migrations/20250213103709_accounting_strings_code_change.sql
+++ migrations/20250213103709_accounting_strings_code_change.sql
@@ -11,6 +11,30 @@ SET account_code = CASE
     END
 WHERE fee_type IN ('MCR', 'MDR');
 
+INSERT INTO transaction_type (fee_type, supervision_level, ledger_type, account_code, description, line_description)
+VALUES
+    ('ZR', 'GA', 'CREDIT REMISSION', 4481102107, 'Remission Credit', 'GA fee reduction'),
+    ('ZR', 'GS', 'CREDIT REMISSION', 4481102107, 'Remission Credit', 'GS fee reduction'),
+    ('ZR', 'GT', 'CREDIT REMISSION', 4481102107, 'Remission Credit', 'GT fee reduction'),
+    ('ZH', 'GA', 'CREDIT HARDSHIP', 4481102107, 'Hardship Credit', 'GA fee reduction'),
+    ('ZH', 'GS', 'CREDIT HARDSHIP', 4481102107, 'Hardship Credit', 'GS fee reduction'),
+    ('ZH', 'GT', 'CREDIT HARDSHIP', 4481102107, 'Hardship Credit', 'GT fee reduction'),
+    ('ZE', 'GA', 'CREDIT EXEMPTION', 4481102108, 'Exemption Credit', 'GA fee reduction'),
+    ('ZE', 'GS', 'CREDIT EXEMPTION', 4481102108, 'Exemption Credit', 'GS fee reduction'),
+    ('ZE', 'GT', 'CREDIT EXEMPTION', 4481102108, 'Exemption Credit', 'GT fee reduction'),
+    ('MCR', 'GA', 'CREDIT MEMO', 4481102107, 'Manual Credit', 'GA Manual credit'),
+    ('MCR', 'GS', 'CREDIT MEMO', 4481102107, 'Manual Credit', 'GS Manual credit'),
+    ('MCR', 'GT', 'CREDIT MEMO', 4481102107, 'Manual Credit', 'GT Manual credit'),
+    ('MDR', 'GA', 'DEBIT MEMO', 4481102107, 'Manual Debit', 'GA Manual debit'),
+    ('MDR', 'GS', 'DEBIT MEMO', 4481102107, 'Manual Credit', 'GS Manual debit'),
+    ('MDR', 'GT', 'DEBIT MEMO', 4481102107, 'Manual Credit', 'GT Manual debit'),
+    ('WO', 'GA', 'CREDIT WRITE OFF', 4481102107, 'Manual Write-off', 'GA Write-off'),
+    ('WO', 'GS', 'CREDIT WRITE OFF', 4481102107, 'Manual Write-off', 'GS Write-off'),
+    ('WO', 'GT', 'CREDIT WRITE OFF', 4481102107, 'Manual Write-off', 'GT Write-off'),
+    ('WOR', 'GA', 'WRITE OFF REVERSAL', 4481102107, 'Write-off reversal', 'GA Write-off reversal'),
+    ('WOR', 'GS', 'WRITE OFF REVERSAL', 4481102107, 'Write-off reversal', 'GS Write-off reversal'),
+    ('WOR', 'GT', 'WRITE OFF REVERSAL', 4481102107, 'Write-off reversal', 'GT Write-off reversal');
+
 -- +goose Down
 
 UPDATE transaction_type
@@ -24,3 +48,6 @@ SET account_code = CASE
                        ELSE account_code
     END
 WHERE fee_type IN ('MCR', 'MDR');
+
+
+DELETE FROM transaction_type WHERE supervision_level IN ('GA', 'GS', 'GT') AND fee_type IN ('ZR', 'ZH', 'ZE', 'MCR', 'MDR', 'WO', 'WOR');
--- migrations/20250227103709_accounting_strings_code_change.sql
+++ migrations/20250227103709_accounting_strings_code_change.sql
@@ -1,27 +1,4 @@
 -- +goose Up
-INSERT INTO transaction_type (fee_type, supervision_level, ledger_type, account_code, description, line_description)
-VALUES
-    ('ZR', 'GA', '', 4481102107, 'Remission Credit', 'GA fee reduction'),
-    ('ZR', 'GS', '', 4481102107, 'Remission Credit', 'GS fee reduction'),
-    ('ZR', 'GT', '', 4481102107, 'Remission Credit', 'GT fee reduction'),
-    ('ZH', 'GA', '', 4481102107, 'Hardship Credit', 'GA fee reduction'),
-    ('ZH', 'GS', '', 4481102107, 'Hardship Credit', 'GS fee reduction'),
-    ('ZH', 'GT', '', 4481102107, 'Hardship Credit', 'GT fee reduction'),
-    ('ZE', 'GA', '', 4481102108, 'Exemption Credit', 'GA fee reduction'),
-    ('ZE', 'GS', '', 4481102108, 'Exemption Credit', 'GS fee reduction'),
-    ('ZE', 'GT', '', 4481102108, 'Exemption Credit', 'GT fee reduction'),
-    ('MCR', 'GA', '', 4481102107, 'Manual Credit', 'GA Manual credit'),
-    ('MCR', 'GS', '', 4481102107, 'Manual Credit', 'GS Manual credit'),
-    ('MCR', 'GT', '', 4481102107, 'Manual Credit', 'GT Manual credit'),
-    ('MDR', 'GA', '', 4481102107, 'Manual Debit', 'GA Manual debit'),
-    ('MDR', 'GS', '', 4481102107, 'Manual Credit', 'GS Manual debit'),
-    ('MDR', 'GT', '', 4481102107, 'Manual Credit', 'GT Manual debit'),
-    ('WO', 'GA', '', 4481102107, 'Manual Write-off', 'GA Write-off'),
-    ('WO', 'GS', '', 4481102107, 'Manual Write-off', 'GS Write-off'),
-    ('WO', 'GT', '', 4481102107, 'Manual Write-off', 'GT Write-off'),
-    ('WOR', 'GA', '', 4481102107, 'Write-off reversal', 'GA Write-off reversal'),
-    ('WOR', 'GS', '', 4481102107, 'Write-off reversal', 'GS Write-off reversal'),
-    ('WOR', 'GT', '', 4481102107, 'Write-off reversal', 'GT Write-off reversal');
+
 
 -- +goose Down
-DELETE FROM transaction_type WHERE supervision_level IN ('GA', 'GS', 'GT') AND fee_type IN ('ZR', 'ZH', 'ZE', 'MCR', 'MDR', 'WO', 'WOR');
