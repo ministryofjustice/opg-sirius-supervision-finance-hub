c2fcbd12c99e4c72139fd528c9040d1d864f6bcf
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2025-03-04 08:22:12 +0000

    PFS-161 Fix guardianship adjustments not showing up

--- migrations/20250213103709_accounting_strings_code_change.sql
+++ migrations/20250213103709_accounting_strings_code_change.sql
@@ -11,29 +11,29 @@ SET account_code = CASE
     END
 WHERE fee_type IN ('MCR', 'MDR');
 
-INSERT INTO transaction_type (fee_type, supervision_level, ledger_type, account_code, description, line_description)
+INSERT INTO transaction_type (fee_type, supervision_level, ledger_type, account_code, description, line_description, is_receipt)
 VALUES
-    ('ZR', 'GA', 'CREDIT REMISSION', 4481102107, 'Remission Credit', 'GA fee reduction'),
-    ('ZR', 'GS', 'CREDIT REMISSION', 4481102107, 'Remission Credit', 'GS fee reduction'),
-    ('ZR', 'GT', 'CREDIT REMISSION', 4481102107, 'Remission Credit', 'GT fee reduction'),
-    ('ZH', 'GA', 'CREDIT HARDSHIP', 4481102107, 'Hardship Credit', 'GA fee reduction'),
-    ('ZH', 'GS', 'CREDIT HARDSHIP', 4481102107, 'Hardship Credit', 'GS fee reduction'),
-    ('ZH', 'GT', 'CREDIT HARDSHIP', 4481102107, 'Hardship Credit', 'GT fee reduction'),
-    ('ZE', 'GA', 'CREDIT EXEMPTION', 4481102108, 'Exemption Credit', 'GA fee reduction'),
-    ('ZE', 'GS', 'CREDIT EXEMPTION', 4481102108, 'Exemption Credit', 'GS fee reduction'),
-    ('ZE', 'GT', 'CREDIT EXEMPTION', 4481102108, 'Exemption Credit', 'GT fee reduction'),
-    ('MCR', 'GA', 'CREDIT MEMO', 4481102107, 'Manual Credit', 'GA Manual credit'),
-    ('MCR', 'GS', 'CREDIT MEMO', 4481102107, 'Manual Credit', 'GS Manual credit'),
-    ('MCR', 'GT', 'CREDIT MEMO', 4481102107, 'Manual Credit', 'GT Manual credit'),
-    ('MDR', 'GA', 'DEBIT MEMO', 4481102107, 'Manual Debit', 'GA Manual debit'),
-    ('MDR', 'GS', 'DEBIT MEMO', 4481102107, 'Manual Credit', 'GS Manual debit'),
-    ('MDR', 'GT', 'DEBIT MEMO', 4481102107, 'Manual Credit', 'GT Manual debit'),
-    ('WO', 'GA', 'CREDIT WRITE OFF', 4481102107, 'Manual Write-off', 'GA Write-off'),
-    ('WO', 'GS', 'CREDIT WRITE OFF', 4481102107, 'Manual Write-off', 'GS Write-off'),
-    ('WO', 'GT', 'CREDIT WRITE OFF', 4481102107, 'Manual Write-off', 'GT Write-off'),
-    ('WOR', 'GA', 'WRITE OFF REVERSAL', 4481102107, 'Write-off reversal', 'GA Write-off reversal'),
-    ('WOR', 'GS', 'WRITE OFF REVERSAL', 4481102107, 'Write-off reversal', 'GS Write-off reversal'),
-    ('WOR', 'GT', 'WRITE OFF REVERSAL', 4481102107, 'Write-off reversal', 'GT Write-off reversal');
+    ('ZR', 'GA', 'CREDIT REMISSION', 4481102107, 'Remission Credit', 'GA fee reduction', false),
+    ('ZR', 'GS', 'CREDIT REMISSION', 4481102107, 'Remission Credit', 'GS fee reduction', false),
+    ('ZR', 'GT', 'CREDIT REMISSION', 4481102107, 'Remission Credit', 'GT fee reduction', false),
+    ('ZH', 'GA', 'CREDIT HARDSHIP', 4481102107, 'Hardship Credit', 'GA fee reduction', false),
+    ('ZH', 'GS', 'CREDIT HARDSHIP', 4481102107, 'Hardship Credit', 'GS fee reduction', false),
+    ('ZH', 'GT', 'CREDIT HARDSHIP', 4481102107, 'Hardship Credit', 'GT fee reduction', false),
+    ('ZE', 'GA', 'CREDIT EXEMPTION', 4481102108, 'Exemption Credit', 'GA fee reduction', false),
+    ('ZE', 'GS', 'CREDIT EXEMPTION', 4481102108, 'Exemption Credit', 'GS fee reduction', false),
+    ('ZE', 'GT', 'CREDIT EXEMPTION', 4481102108, 'Exemption Credit', 'GT fee reduction', false),
+    ('MCR', 'GA', 'CREDIT MEMO', 4481102107, 'Manual Credit', 'GA Manual credit', false),
+    ('MCR', 'GS', 'CREDIT MEMO', 4481102107, 'Manual Credit', 'GS Manual credit', false),
+    ('MCR', 'GT', 'CREDIT MEMO', 4481102107, 'Manual Credit', 'GT Manual credit', false),
+    ('MDR', 'GA', 'DEBIT MEMO', 4481102107, 'Manual Debit', 'GA Manual debit', false),
+    ('MDR', 'GS', 'DEBIT MEMO', 4481102107, 'Manual Credit', 'GS Manual debit', false),
+    ('MDR', 'GT', 'DEBIT MEMO', 4481102107, 'Manual Credit', 'GT Manual debit', false),
+    ('WO', 'GA', 'CREDIT WRITE OFF', 4481102107, 'Manual Write-off', 'GA Write-off', false),
+    ('WO', 'GS', 'CREDIT WRITE OFF', 4481102107, 'Manual Write-off', 'GS Write-off', false),
+    ('WO', 'GT', 'CREDIT WRITE OFF', 4481102107, 'Manual Write-off', 'GT Write-off', false),
+    ('WOR', 'GA', 'WRITE OFF REVERSAL', 4481102107, 'Write-off reversal', 'GA Write-off reversal', false),
+    ('WOR', 'GS', 'WRITE OFF REVERSAL', 4481102107, 'Write-off reversal', 'GS Write-off reversal', false),
+    ('WOR', 'GT', 'WRITE OFF REVERSAL', 4481102107, 'Write-off reversal', 'GT Write-off reversal', false);
 
 -- +goose Down
 
