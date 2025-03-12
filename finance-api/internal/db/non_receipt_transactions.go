ff573aa61533455b6bfbf9d69d8081cd7055c99a
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2025-03-12 13:13:19 +0000

    PFS-161 Use transaction type ID to order results rather than line description

--- finance-api/internal/db/non_receipt_transactions.go
+++ finance-api/internal/db/non_receipt_transactions.go
@@ -9,7 +9,61 @@ type NonReceiptTransactions struct {
 }
 
 const NonReceiptTransactionsQuery = `
-WITH transactions AS (
+WITH transaction_type_order AS (
+	SELECT 
+		id, 
+		CASE WHEN id = 1 THEN 1 
+		WHEN id = 2 THEN 2
+		WHEN id = 3 THEN 3
+		WHEN id = 4 THEN 4
+		WHEN id = 5 THEN 5
+		WHEN id = 6 THEN 6
+		WHEN id = 7 THEN 7
+		WHEN id = 8 THEN 8
+		WHEN id = 9 THEN 9
+		WHEN id = 1 THEN 10
+		WHEN id = 11 THEN 11
+		WHEN id = 33 THEN 12
+		WHEN id = 34 THEN 13
+		WHEN id = 35 THEN 14
+		WHEN id IN (12, 13, 14) THEN 15
+		WHEN id IN (19, 20, 21) THEN 16
+		WHEN id IN (26, 27, 28) THEN 17
+		WHEN id IN (49, 52) THEN 18
+		WHEN id IN (50, 53) THEN 19
+		WHEN id IN (51, 54) THEN 20
+		WHEN id = 55 THEN 21
+		WHEN id = 56 THEN 22
+		WHEN id = 57 THEN 23
+		WHEN id = 15 THEN 24
+		WHEN id = 22 THEN 25
+		WHEN id = 29 THEN 26
+		WHEN id = 58 THEN 27
+		WHEN id = 59 THEN 28
+		WHEN id = 60 THEN 29
+		WHEN id = 16 THEN 30
+		WHEN id = 23 THEN 31
+		WHEN id = 30 THEN 32
+		WHEN id = 61 THEN 33
+		WHEN id = 62 THEN 34
+		WHEN id = 63 THEN 35
+		WHEN id = 17 THEN 36
+		WHEN id = 24 THEN 37
+		WHEN id = 31 THEN 38
+		WHEN id = 64 THEN 39
+		WHEN id = 65 THEN 40
+		WHEN id = 66 THEN 41
+		WHEN id = 18 THEN 42
+		WHEN id = 25 THEN 43
+		WHEN id = 32 THEN 44
+		WHEN id = 67 THEN 45
+		WHEN id = 68 THEN 46
+		WHEN id = 69 THEN 47
+		ELSE 48
+		END AS index
+	FROM transaction_type WHERE is_receipt = false
+),
+transactions AS (
    	SELECT
 		l.created_at::DATE AS created_at,
 		l.type AS ledger_type,
@@ -38,6 +92,7 @@ transaction_totals AS (
 		ABS(SUM(t.amount) / 100.0)::NUMERIC(10,2)::VARCHAR(255) AS amount,
 		account.cost_centre,
 		tt.is_credit,
+		tt.index,
 		n
 	FROM transactions t
 	LEFT JOIN LATERAL (
@@ -48,102 +103,44 @@ transaction_totals AS (
 		LIMIT 1
 	) sl ON TRUE
 	LEFT JOIN LATERAL (
-		SELECT fee_type, account_code, line_description, is_receipt, CASE WHEN fee_type IN ('MCR', 'ZR', 'ZE', 'ZH', 'WO') THEN true ELSE false END AS is_credit
+		SELECT 
+			tto.index, fee_type, account_code, line_description, 
+			CASE WHEN fee_type IN ('MCR', 'ZR', 'ZE', 'ZH', 'WO') THEN true ELSE false END AS is_credit
 		FROM transaction_type tt
-		WHERE (tt.ledger_type = t.ledger_type OR tt.fee_type = t.fee_type)
+		LEFT JOIN transaction_type_order tto ON tt.id = tto.id
+		WHERE (tt.ledger_type = t.ledger_type OR tt.fee_type = t.fee_type) 
+		AND is_receipt = false
 		AND sl.supervision_level = tt.supervision_level
-		ORDER BY id DESC
 	) tt ON TRUE
 	LEFT JOIN account ON tt.account_code = account.code
 	CROSS JOIN (select 1 as n union all select 2) n
-	WHERE tt.is_receipt = false
-	GROUP BY tt.line_description, t.created_at, tt.account_code, account.cost_centre, tt.is_credit, n
+	GROUP BY tt.line_description, t.created_at, tt.account_code, account.cost_centre, tt.is_credit, tt.index, n 
 )
 SELECT
     '="0470"' AS "Entity",
-    CASE
-        WHEN n % 2 = 1 THEN
-            cost_centre
-        ELSE
-            '99999999'
+    CASE WHEN n % 2 = 1 
+		THEN cost_centre
+        ELSE '99999999'
         END AS "Cost Centre",
-    CASE
-        WHEN n % 2 = 1 THEN
-            account_code
-        ELSE
-            '1816100000'
+    CASE WHEN n % 2 = 1 
+		THEN account_code
+        ELSE '1816100000'
         END AS "Account",
     '="0000000"' AS "Objective",
     '="00000000"' AS "Analysis",
     '="0000"' AS "Intercompany",
     '="00000000"' AS "Spare",
-    CASE
-        WHEN n % 2 = 1 AND is_credit = false OR n % 2 = 0 AND is_credit THEN
-            ''
-        ELSE
-            amount
+    CASE WHEN n % 2 = 1 AND is_credit = false OR n % 2 = 0 AND is_credit 
+		THEN ''
+        ELSE amount
         END AS "Debit",
-    CASE
-        WHEN n % 2 = 1 AND is_credit = false OR n % 2 = 0 AND is_credit THEN
-            amount
-        ELSE
-            ''
+    CASE WHEN n % 2 = 1 AND is_credit = false OR n % 2 = 0 AND is_credit 
+		THEN amount
+        ELSE ''
         END AS "Credit",
     line_description || ' [' || TO_CHAR(transaction_date, 'DD/MM/YYYY') || ']' AS "Line description"
-FROM
-    transaction_totals
-ORDER BY
-    CASE
-        WHEN line_description LIKE 'AD invoice%' THEN 1
-        WHEN line_description LIKE 'S2 invoice%' THEN 2
-        WHEN line_description LIKE 'S3 invoice%' THEN 3
-        WHEN line_description LIKE 'B2 invoice%' THEN 4
-        WHEN line_description LIKE 'B3 invoice%' THEN 5
-        WHEN line_description LIKE 'Gen SF invoice%' THEN 6
-        WHEN line_description LIKE 'Min SF invoice%' THEN 7
-        WHEN line_description LIKE 'Gen SE invoice%' THEN 8
-        WHEN line_description LIKE 'Min SE invoice%' THEN 9
-        WHEN line_description LIKE 'Gen SO invoice%' THEN 10
-        WHEN line_description LIKE 'Min SO invoice%' THEN 11
-		WHEN line_description LIKE 'GA invoice%' THEN 12
-		WHEN line_description LIKE 'GS invoice%' THEN 13
-		WHEN line_description LIKE 'GT invoice%' THEN 14
-        WHEN line_description LIKE 'AD Rem/Exem%' THEN 15
-        WHEN line_description LIKE 'Gen Rem/Exem%' THEN 16
-        WHEN line_description LIKE 'Min Rem/Exem%' THEN 17
-		WHEN line_description LIKE 'GA remissions & hardships%' THEN 18
-		WHEN line_description LIKE 'GS remissions & hardships%' THEN 19
-		WHEN line_description LIKE 'GT remissions & hardships%' THEN 20
-		WHEN line_description LIKE 'GA exemptions%' THEN 21
-		WHEN line_description LIKE 'GS exemptions%' THEN 22
-		WHEN line_description LIKE 'GT exemptions%' THEN 23
-        WHEN line_description LIKE 'AD Manual credit%' THEN 24
-        WHEN line_description LIKE 'Gen Manual credit%' THEN 25
-        WHEN line_description LIKE 'Min Manual credit%' THEN 26
-        WHEN line_description LIKE 'GA Manual credit%' THEN 27
-        WHEN line_description LIKE 'GS Manual credit%' THEN 28
-        WHEN line_description LIKE 'GT Manual credit%' THEN 29
-        WHEN line_description LIKE 'AD Manual debit%' THEN 30
-        WHEN line_description LIKE 'Gen Manual debit%' THEN 31
-        WHEN line_description LIKE 'Min Manual debit%' THEN 32
-        WHEN line_description LIKE 'GA Manual debit%' THEN 33
-        WHEN line_description LIKE 'GS Manual debit%' THEN 34
-        WHEN line_description LIKE 'GT Manual debit%' THEN 35
-        WHEN line_description LIKE 'AD Write-off%' AND line_description NOT LIKE 'AD Write-off reversal%' THEN 36
-        WHEN line_description LIKE 'Gen Write-off%' AND line_description NOT LIKE 'Gen Write-off reversal%' THEN 37
-        WHEN line_description LIKE 'Min Write-off%' AND line_description NOT LIKE 'Min Write-off reversal%' THEN 38
-        WHEN line_description LIKE 'GA Write-off%' AND line_description NOT LIKE 'GA Write-off reversal%' THEN 39
-        WHEN line_description LIKE 'GS Write-off%' AND line_description NOT LIKE 'GS Write-off reversal%' THEN 40
-        WHEN line_description LIKE 'GT Write-off%' AND line_description NOT LIKE 'GT Write-off reversal%' THEN 41
-        WHEN line_description LIKE 'AD Write-off reversal%' THEN 42
-        WHEN line_description LIKE 'Gen Write-off reversal%' THEN 43
-        WHEN line_description LIKE 'Min Write-off reversal%' THEN 44
-        WHEN line_description LIKE 'GA Write-off reversal%' THEN 45
-        WHEN line_description LIKE 'GS Write-off reversal%' THEN 46
-        WHEN line_description LIKE 'GT Write-off reversal%' THEN 47
-        ELSE 48
-        END, n;
-`
+FROM transaction_totals
+ORDER BY index, n;`
 
 func (n *NonReceiptTransactions) GetHeaders() []string {
 	return []string{
