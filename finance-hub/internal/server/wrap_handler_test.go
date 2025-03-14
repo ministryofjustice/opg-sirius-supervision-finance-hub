c9b14a3af4aacc4847994a201f2c44d816062014
Sam Joseph <denialanderror@users.noreply.github.com>
2025-03-14 09:52:59 +0000

    pfs-264: Adds user id to request logs for better tracing through the services (#249)

--- finance-hub/internal/server/wrap_handler_test.go
+++ finance-hub/internal/server/wrap_handler_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ministryofjustice/opg-go-common/telemetry"
 	"github.com/ministryofjustice/opg-sirius-supervision-finance-hub/finance-hub/internal/api"
 	"github.com/ministryofjustice/opg-sirius-supervision-finance-hub/finance-hub/internal/auth"
+	"github.com/ministryofjustice/opg-sirius-supervision-finance-hub/shared"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -42,7 +43,10 @@ func (m *mockHandler) render(app AppVars, w http.ResponseWriter, r *http.Request
 
 func Test_wrapHandler_successful_request(t *testing.T) {
 	w := httptest.NewRecorder()
-	ctx := auth.Context{Context: telemetry.ContextWithLogger(context.Background(), telemetry.NewLogger("opg-sirius-supervision-finance-hub"))}
+	ctx := auth.Context{
+		Context: telemetry.ContextWithLogger(context.Background(), telemetry.NewLogger("opg-sirius-supervision-finance-hub")),
+		User:    &shared.User{ID: 1},
+	}
 	r, _ := http.NewRequestWithContext(ctx, http.MethodGet, "test-url/1", nil)
 
 	errorTemplate := &mockTemplate{}
@@ -80,7 +84,10 @@ func Test_wrapHandler_status_error_handling(t *testing.T) {
 	for i, test := range tests {
 		t.Run("Scenario "+strconv.Itoa(i), func(t *testing.T) {
 			w := httptest.NewRecorder()
-			ctx := auth.Context{Context: telemetry.ContextWithLogger(context.Background(), telemetry.NewLogger("opg-sirius-supervision-finance-hub"))}
+			ctx := auth.Context{
+				Context: telemetry.ContextWithLogger(context.Background(), telemetry.NewLogger("opg-sirius-supervision-finance-hub")),
+				User:    &shared.User{ID: 1},
+			}
 			r, _ := http.NewRequestWithContext(ctx, http.MethodGet, "test-url/1", nil)
 
 			errorTemplate := &mockTemplate{error: errors.New("some template error")}
