cea80cd66a640e9bff895807c772a006e5073b17
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2025-03-18 15:34:17 +0000

    PFS-171 Fix failing test

--- finance-api/internal/reports/generate_report_test.go
+++ finance-api/internal/reports/generate_report_test.go
@@ -362,7 +362,7 @@ func TestSendSuccessNotification(t *testing.T) {
 		EmailAddress: emailAddress,
 		TemplateId:   reportRequestedTemplateId,
 		Personalisation: reportRequestedNotifyPersonalisation{
-			FileLink:          "http://example.com/download?uid=eyJLZXkiOiJ0ZXN0X3JlcG9ydC5jc3YiLCJWZXJzaW9uSWQiOiIxMjM0NSJ9",
+			FileLink:          "http://example.com/download?uid=eyJLZXkiOiJ0ZXN0X3JlcG9ydC5jc3YiLCJWZXJzaW9uSWQiOiIxMjM0NSIsIkJ1Y2tldCI6IiJ9",
 			ReportName:        reportName,
 			RequestedDate:     "2024-01-01",
 			RequestedDateTime: "2024-01-01 00:00:00",
