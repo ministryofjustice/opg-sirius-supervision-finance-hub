9bc9831c8f7a57a2ec8718db7dd2d3573e962710
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2025-03-18 08:36:57 +0000

    PFS-171 Add condition based on file key

--- finance-api/cmd/api/download.go
+++ finance-api/cmd/api/download.go
@@ -24,7 +24,12 @@ func (s *Server) download(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	var result *s3.GetObjectOutput
-	result, err = s.fileStorage.GetFileByVersion(ctx, downloadRequest.Bucket, downloadRequest.Key, downloadRequest.VersionId)
+
+	if downloadRequest.Key == "FeeAccrual.csv" {
+		result, err = s.fileStorage.GetFile(ctx, downloadRequest.Bucket, downloadRequest.Key)
+	} else {
+		result, err = s.fileStorage.GetFileByVersion(ctx, downloadRequest.Bucket, downloadRequest.Key, downloadRequest.VersionId)
+	}
 
 	if err != nil {
 		var apiErr smithy.APIError
