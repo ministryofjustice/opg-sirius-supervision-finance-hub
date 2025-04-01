58977fb82f7200128f30923638db2626b34177e1
nicholassully <nicholas.sully@digital.justice.gov.uk>
2025-04-01 10:58:33 +0100

    pfs-299 adding more logging in for errors

--- finance-api/internal/service/process_finance_admin_upload.go
+++ finance-api/internal/service/process_finance_admin_upload.go
@@ -5,11 +5,11 @@ import (
 	"encoding/csv"
 	"fmt"
 	"github.com/jackc/pgx/v5/pgtype"
-	"github.com/ministryofjustice/opg-go-common/telemetry"
 	"github.com/ministryofjustice/opg-sirius-supervision-finance-hub/finance-api/internal/auth"
 	"github.com/ministryofjustice/opg-sirius-supervision-finance-hub/finance-api/internal/notify"
 	"github.com/ministryofjustice/opg-sirius-supervision-finance-hub/finance-api/internal/store"
 	"github.com/ministryofjustice/opg-sirius-supervision-finance-hub/shared"
+	"log/slog"
 	"os"
 	"slices"
 	"strconv"
@@ -19,11 +19,10 @@ import (
 
 func (s *Service) ProcessFinanceAdminUpload(ctx context.Context, detail shared.FinanceAdminUploadEvent) error {
 	file, err := s.fileStorage.GetFile(ctx, os.Getenv("ASYNC_S3_BUCKET"), detail.Filename)
-	logger := telemetry.LoggerFromContext(ctx)
-	logger.Info("processing file " + detail.Filename)
+	s.Logger(ctx).Info("processing file " + detail.Filename)
 
 	if err != nil {
-		logger.Error("unable to download report", "error", err)
+		s.Logger(ctx).Error("Error unable to download report", slog.String("err", err.Error()))
 		payload := createUploadNotifyPayload(detail, fmt.Errorf("unable to download report"), map[int]string{})
 		return s.notify.Send(ctx, payload)
 	}
@@ -31,7 +30,7 @@ func (s *Service) ProcessFinanceAdminUpload(ctx context.Context, detail shared.F
 	csvReader := csv.NewReader(file)
 	records, err := csvReader.ReadAll()
 	if err != nil {
-		logger.Error("unable to read report", "error", err)
+		s.Logger(ctx).Error("Error unable to read report", slog.String("err", err.Error()))
 		payload := createUploadNotifyPayload(detail, fmt.Errorf("unable to read report"), map[int]string{})
 		return s.notify.Send(ctx, payload)
 	}
