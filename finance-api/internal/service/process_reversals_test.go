a45b7019144466e9dd73ebacca9f3e9dd187a229
Sam Joseph <denialanderror@googlemail.com>
2025-04-09 12:59:18 +0100

    pfs-234: Adds translations

--- finance-api/internal/service/process_reversals_test.go
+++ finance-api/internal/service/process_reversals_test.go
@@ -108,7 +108,7 @@ func (suite *IntegrationSuite) Test_processReversals() {
 			},
 			expectedFailedLines: map[int]string{
 				1: "NO_MATCHED_PAYMENT",
-				2: "COURT_REF_MISMATCH",
+				2: "CLIENT_NOT_FOUND",
 				3: "NO_MATCHED_PAYMENT",
 				4: "NO_MATCHED_PAYMENT",
 				5: "NO_MATCHED_PAYMENT",
