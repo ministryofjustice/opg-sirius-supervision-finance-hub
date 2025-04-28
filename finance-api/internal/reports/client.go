4a7f3a200a29e1e3ad1228fbb1cb25999740f57f
Sam Joseph <denialanderror@googlemail.com>
2025-04-28 15:10:31 +0100

    pfs-315: Tries to stream. Localstack not happy

--- finance-api/internal/reports/client.go
+++ finance-api/internal/reports/client.go
@@ -1,22 +1,22 @@
 package reports
 
 import (
-	"bytes"
 	"context"
-	"encoding/csv"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/ministryofjustice/opg-sirius-supervision-finance-hub/finance-api/internal/db"
 	"github.com/ministryofjustice/opg-sirius-supervision-finance-hub/finance-api/internal/notify"
+	"io"
 	"time"
 )
 
 type dbClient interface {
 	Run(ctx context.Context, query db.ReportQuery) ([][]string, error)
+	CopyStream(ctx context.Context, query db.ReportQuery) (io.ReadCloser, error)
 	Close()
 }
 
 type fileStorageClient interface {
-	PutFile(ctx context.Context, bucketName string, fileName string, data *bytes.Buffer) (*string, error)
+	StreamFile(ctx context.Context, bucketName string, fileName string, stream io.ReadCloser) (*string, error)
 }
 
 type notifyClient interface {
@@ -49,31 +49,6 @@ func NewClient(dbPool *pgxpool.Pool, fileStorage fileStorageClient, notify notif
 	}
 }
 
-func (c *Client) generate(ctx context.Context, query db.ReportQuery) (*bytes.Buffer, error) {
-	rows, err := c.db.Run(ctx, query)
-	if err != nil {
-		return nil, err
-	}
-
-	return createCsv(rows)
-}
-
-func createCsv(items [][]string) (*bytes.Buffer, error) {
-	var buffer bytes.Buffer
-
-	writer := csv.NewWriter(&buffer)
-
-	for _, item := range items {
-		err := writer.Write(item)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	writer.Flush()
-	if writer.Error() != nil {
-		return nil, writer.Error()
-	}
-
-	return &buffer, nil
+func (c *Client) stream(ctx context.Context, query db.ReportQuery) (io.ReadCloser, error) {
+	return c.db.CopyStream(ctx, query)
 }
