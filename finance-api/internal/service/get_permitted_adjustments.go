501cef82e8650bbd3880ee1baf1caa23e00067a2
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2025-06-10 19:30:40 +0100

    PFS-364 Create invoice adjustment type & check if permitted

--- finance-api/internal/service/get_permitted_adjustments.go
+++ finance-api/internal/service/get_permitted_adjustments.go
@@ -27,5 +27,19 @@ func (s *Service) GetPermittedAdjustments(ctx context.Context, invoiceId int32) 
 		permitted = append(permitted, shared.AdjustmentTypeWriteOffReversal)
 	}
 
+	feeReductionDetails, err := s.store.GetInvoiceFeeReductionReversalDetails(ctx, invoiceId)
+	if err != nil {
+		s.Logger(ctx).Error("Error get invoice fee reduction details in get permitted adjustments", slog.String("err", err.Error()))
+		return nil, err
+	}
+
+	// add condition for fee reduction reversal
+	// if there is an existing fee reduction credit on the invoice and there is not an equivalent fee reduction reversal for that fee reductio
+	// (calculated by the total of fee reduction credits + total of fee reduction reversal debits being more than zero)
+
+	if feeReductionDetails.ReversalTotal < feeReductionDetails.FeeReductionTotal {
+		permitted = append(permitted, shared.AdjustmentTypeFeeReductionReversal)
+	}
+
 	return permitted, nil
 }
