66f564337cffd944957f811b48ea7c725bd0c196
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2025-06-26 12:20:00 +0100

    PFS-243 Attempt to fix stream file

--- finance-api/internal/service/process_direct_upload.go
+++ finance-api/internal/service/process_direct_upload.go
@@ -5,8 +5,15 @@ import (
 	"io"
 )
 
+type FileReader struct {
+	io.Reader
+}
+
+func (cb *FileReader) Close() (err error) { return nil }
+
 func (s *Service) ProcessDirectUploadReport(ctx context.Context, filename string, fileBytes io.Reader) error {
-	_, err := s.fileStorage.StreamFile(ctx, s.env.AsyncBucket, filename, io.NopCloser(fileBytes))
+	file := FileReader{fileBytes}
+	_, err := s.fileStorage.StreamFile(ctx, s.env.AsyncBucket, filename, &file)
 	if err != nil {
 		return err
 	}
