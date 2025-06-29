c5855f5a1da835c178b3219e321a32630ea2f201
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2025-06-29 13:12:06 +0100

    PFS-243 Attempt to seek to the beginning of file before upload

--- finance-api/internal/service/process_direct_upload.go
+++ finance-api/internal/service/process_direct_upload.go
@@ -6,6 +6,13 @@ import (
 )
 
 func (s *Service) ProcessDirectUploadReport(ctx context.Context, filename string, fileBytes io.Reader) error {
+	if seeker, ok := fileBytes.(io.Seeker); ok {
+		_, err := seeker.Seek(0, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+
 	_, err := s.fileStorage.StreamFile(ctx, s.env.AsyncBucket, filename, fileBytes)
 	if err != nil {
 		return err
