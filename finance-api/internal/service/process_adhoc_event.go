f6f768907c278be0f96d47465c053fbf39b2d881
Sam Joseph <denialanderror@googlemail.com>
2025-07-08 11:31:27 +0100

    pfs-384: Adhoc task to rebalance CCB using the reapply function for all clients with credit

--- finance-api/internal/service/process_adhoc_event.go
+++ finance-api/internal/service/process_adhoc_event.go
@@ -2,9 +2,28 @@ package service
 
 import (
 	"context"
-	"errors"
+	"fmt"
 )
 
 func (s *Service) ProcessAdhocEvent(ctx context.Context) error {
-	return errors.New("no adhoc event implemented")
+	logger := s.Logger(ctx)
+	logger.Info("RebalanceCCB: process adhoc event")
+
+	clientIDs, err := s.store.GetClientsWithCredit(ctx)
+	if err != nil {
+		return err
+	}
+
+	logger.Info(fmt.Sprintf("RebalanceCCB: %d clients found", len(clientIDs)))
+
+	for _, id := range clientIDs {
+		err := s.ReapplyCredit(ctx, id, nil)
+		if err != nil {
+			logger.Error(fmt.Sprintf("RebalanceCCB: unable to reapply for client with id %d", id), "error", err)
+			return err
+		}
+	}
+
+	logger.Info("RebalanceCCB: run successfully")
+	return nil
 }
