50235b1d34977a20e5292640fdd2e0dfe08c5cf9
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2025-07-09 11:07:36 +0100

    PFS-381 Add remaining supervision levels for transaction type

--- migrations/20250708193252_migrations_fee_reduction_reversals_on_journal.sql
+++ migrations/20250708193252_migrations_fee_reduction_reversals_on_journal.sql
@@ -1,7 +1,13 @@
 -- +goose Up
 INSERT INTO transaction_type (fee_type, supervision_level, ledger_type, account_code, description, line_description, is_receipt)
 VALUES
-       ('FRR', 'AD', 'FEE REDUCTION REVERSAL', 4481102107, 'Fee Reduction Reversal', 'AD Fee reduction reversal', false);
+       ('FRR', 'AD', 'FEE REDUCTION REVERSAL', 4481102107, 'Fee Reduction Reversal', 'AD Fee reduction reversal', false),
+       ('FRR', 'GENERAL', 'FEE REDUCTION REVERSAL', 4481102107, 'Fee Reduction Reversal', 'General Fee reduction reversal', false),
+       ('FRR', 'MINIMAL', 'FEE REDUCTION REVERSAL', 4481102107, 'Fee Reduction Reversal', 'Minimal Fee reduction reversal', false),
+       ('FRR', 'GA', 'FEE REDUCTION REVERSAL', 4481102107, 'Fee Reduction Reversal', 'GA Fee reduction reversal', false),
+       ('FRR', 'GT', 'FEE REDUCTION REVERSAL', 4481102107, 'Fee Reduction Reversal', 'GT Fee reduction reversal', false),
+       ('FRR', 'GS', 'FEE REDUCTION REVERSAL', 4481102107, 'Fee Reduction Reversal', 'GS Fee reduction reversal', false);
 
 
 -- +goose Down
+DELETE FROM transaction_type WHERE fee_type = 'FRR';
