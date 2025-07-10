6b945baabc8747de5d6d07b9a083ed5149305bfa
Sam Joseph <denialanderror@googlemail.com>
2025-07-10 13:16:52 +0100

    pfs-384: Make process async to avoid context cancelation

--- finance-api/internal/service/process_adhoc_event.go
+++ finance-api/internal/service/process_adhoc_event.go
@@ -3,27 +3,37 @@ package service
 import (
 	"context"
 	"fmt"
+	"github.com/ministryofjustice/opg-go-common/telemetry"
+	"github.com/ministryofjustice/opg-sirius-supervision-finance-hub/finance-api/internal/auth"
+	"log/slog"
 )
 
 func (s *Service) ProcessAdhocEvent(ctx context.Context) error {
-	logger := s.Logger(ctx)
-	logger.Info("RebalanceCCB: process adhoc event")
-
-	clientIDs, err := s.store.GetClientsWithCredit(ctx)
-	if err != nil {
-		return err
-	}
-
-	logger.Info(fmt.Sprintf("RebalanceCCB: %d clients found", len(clientIDs)))
-
-	for _, id := range clientIDs {
-		err := s.ReapplyCredit(ctx, id, nil)
-		if err != nil {
-			logger.Error(fmt.Sprintf("RebalanceCCB: unable to reapply for client with id %d", id), "error", err)
-			return err
+	// perform async so request context doesn't cancel before process is complete
+	go func(logger *slog.Logger) {
+		logger.Info("RebalanceCCB: process adhoc event")
+		funcCtx := telemetry.ContextWithLogger(context.Background(), logger)
+		funcCtx = auth.Context{
+			Context: funcCtx,
+			User:    ctx.(auth.Context).User,
 		}
-	}
+		clientIDs, err := s.store.GetClientsWithCredit(funcCtx)
+		if err != nil {
+			logger.Error("RebalanceCCB: unable to fetch clients", "error", err)
+			return
+		}
 
-	logger.Info("RebalanceCCB: run successfully")
+		logger.Info(fmt.Sprintf("RebalanceCCB: %d clients found", len(clientIDs)))
+
+		for _, id := range clientIDs {
+			err := s.ReapplyCredit(funcCtx, id, nil)
+			if err != nil {
+				logger.Error(fmt.Sprintf("RebalanceCCB: unable to reapply for client with id %d", id), "error", err)
+			}
+		}
+
+		logger.Info("RebalanceCCB: run successfully")
+	}(telemetry.LoggerFromContext(ctx))
+
 	return nil
 }
