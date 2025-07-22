80054993eb109edca4cda122197106dc6499811d
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2025-07-22 14:08:44 +0100

    SFS-5 Create cancel direct debit files

---
+++ finance-api/cmd/api/cancel_direct_debit.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"net/http"
+)
+
+func (s *Server) cancelDirectDebit(w http.ResponseWriter, r *http.Request) error {
+
+	//call all pay here
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	return nil
+}
