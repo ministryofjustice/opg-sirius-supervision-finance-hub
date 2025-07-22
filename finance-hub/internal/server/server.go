ced55b0a15d7a1f7d4e7dc010eb79c7c19324cfa
Kate Smith <kate.smith@digital.justice.gov.uk>
2025-07-22 10:19:01 +0100

    sfs-49 add direct debit feature flag

--- finance-hub/internal/server/server.go
+++ finance-hub/internal/server/server.go
@@ -50,14 +50,15 @@ type HtmxHandler interface {
 }
 
 type Envs struct {
-	Port            string
-	WebDir          string
-	SiriusURL       string
-	SiriusPublicURL string
-	Prefix          string
-	BackendURL      string
-	BillingTeamID   int
-	ShowRefunds     bool
+	Port             string
+	WebDir           string
+	SiriusURL        string
+	SiriusPublicURL  string
+	Prefix           string
+	BackendURL       string
+	BillingTeamID    int
+	ShowRefunds      bool
+	ShowDirectDebits bool
 }
 
 func New(logger *slog.Logger, client *api.Client, templates map[string]*template.Template, envs Envs) http.Handler {
