3ae04d2b00a75e3da9e1c817c2b675e59155af9e
Kate Smith <52196833+kate-49@users.noreply.github.com>
2025-07-24 10:04:28 +0100

    sfs-49 remove radio button and account Holder content (#372)

--- shared/add_direct_debit.go
+++ shared/add_direct_debit.go
@@ -1,7 +1,6 @@
 package shared
 
 type AddDirectDebit struct {
-	AccountHolder string `json:"accountHolder" validate:"required"`
 	AccountName   string `json:"name" validate:"required"`
 	SortCode      string `json:"sortCode" validate:"required,len=8"`
 	AccountNumber string `json:"account" validate:"required,numeric,len=8"`
