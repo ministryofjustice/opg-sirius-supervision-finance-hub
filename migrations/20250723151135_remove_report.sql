1fe9cdf7a435380dcc927936e4ea12f98dfcf96b
Sam Joseph <denialanderror@googlemail.com>
2025-07-30 16:00:04 +0100

    sfs-130: Adds if exists to migration in case it gets duplicated in opg-sirius in error

--- migrations/20250723151135_remove_report.sql
+++ migrations/20250723151135_remove_report.sql
@@ -1,6 +1,6 @@
 -- +goose Up
-DROP SEQUENCE report_id_seq CASCADE;
-DROP TABLE report;
+DROP SEQUENCE IF EXISTS report_id_seq CASCADE;
+DROP TABLE IF EXISTS report;
 
 -- +goose Down
 -- no down migration for dropping a table
