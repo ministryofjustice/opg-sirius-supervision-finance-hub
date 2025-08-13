0dfc196ac38a65ad2f7b61df347067e15db4947b
Sam Joseph <denialanderror@googlemail.com>
2025-08-13 15:38:49 +0100

    sfs-7: Cancel direct debit instruction via Allpay

--- finance-hub/internal/server/server.go
+++ finance-hub/internal/server/server.go
@@ -15,8 +15,8 @@ import (
 )
 
 type ApiClient interface {
+	CancelDirectDebitMandate(context.Context, int) error
 	CreateDirectDebitMandate(context.Context, int, api.AccountDetails) error
-	CancelDirectDebit(context.Context, int) error
 	AddFeeReduction(context.Context, int, string, string, string, string, string) error
 	AddInvoiceAdjustment(context.Context, int, int, int, string, string, string, bool) error
 	AddManualInvoice(context.Context, int, string, *string, *string, *string, *string, *string, *string) error
