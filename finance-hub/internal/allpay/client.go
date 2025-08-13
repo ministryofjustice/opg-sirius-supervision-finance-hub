5ef665acd294a8e334cf022de92afd74d898170f
Sam Joseph <denialanderror@users.noreply.github.com>
2025-08-13 10:52:06 +0100

    SFS-145: Create DD mandate (#376)
    * sfs-145: Initial create mandate validation
    
    * sfs-145: Allpay client written
    
    * sfs-145: Create mandate flow written. Happy path test created.
    
    * sfs-145: Tests written and linted. TestLogger added for asserting on log
    messages.
    
    * sfs-145: Cypress tests
    
    * sfs-145: Make allpay env vars optional until we have an API or mock in environments to prevent the service failing to start
    
    * sfs-145: PR fixes

---
+++ finance-hub/internal/allpay/client.go
@@ -0,0 +1,58 @@
+package allpay
+
+import (
+	"context"
+	"github.com/ministryofjustice/opg-sirius-supervision-finance-hub/finance-hub/internal/auth"
+	"io"
+	"net/http"
+)
+
+type Envs struct {
+	apiHost    string
+	apiKey     string
+	schemeCode string
+}
+
+type Client struct {
+	http HTTPClient
+	Envs
+}
+
+func NewClient(httpClient HTTPClient, apiHost string, apiKey string, schemeCode string) *Client {
+	return &Client{
+		http: httpClient,
+		Envs: Envs{
+			apiHost:    apiHost,
+			apiKey:     apiKey,
+			schemeCode: schemeCode,
+		},
+	}
+}
+
+type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+func (c *Client) newRequest(ctx context.Context, method, path string, body io.Reader) (*http.Request, error) {
+	req, err := http.NewRequestWithContext(ctx, method, c.apiHost+"/AllpayApi"+path, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+c.apiKey)
+	req.Header.Add("Accept", "application/json")
+
+	for _, c := range ctx.(auth.Context).Cookies {
+		if c.Name == "x-test-prefer" && c.Value != "" { // used for testing
+			req.Header.Add("Prefer", c.Value)
+		}
+	}
+
+	return req, err
+}
+
+// unchecked allows errors to be unchecked when deferring a function, e.g. closing a reader where a failure would only
+// occur when the process is likely to already be unrecoverable
+func unchecked(f func() error) {
+	_ = f()
+}
