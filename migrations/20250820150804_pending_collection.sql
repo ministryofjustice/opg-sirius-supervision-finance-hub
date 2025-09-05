b140e21936421a004500abb12254f188bd6c922c
Sam Joseph <denialanderror@googlemail.com>
2025-09-05 11:57:36 +0100

    sfs-51: Adds indexes to pending collection to improve query cost

--- migrations/20250820150804_pending_collection.sql
+++ migrations/20250820150804_pending_collection.sql
@@ -1,18 +1,22 @@
 -- +goose Up
 CREATE TABLE pending_collection
 (
-    id                INT       NOT NULL PRIMARY KEY,
-    finance_client_id INT       NOT NULL REFERENCES finance_client (id),
+    id                INTEGER   NOT NULL PRIMARY KEY,
+    finance_client_id INTEGER REFERENCES finance_client (id),
     collection_date   DATE      NOT NULL,
-    amount            INT       NOT NULL,
+    amount            INTEGER   NOT NULL,
     status            VARCHAR   NOT NULL,
-    ledger_id         INT,
+    ledger_id         INTEGER REFERENCES ledger (id),
     created_at        TIMESTAMP NOT NULL,
-    created_by        INT       NOT NULL
+    created_by        INTEGER   NOT NULL
 );
 
+CREATE INDEX idx_pending_collection_client_id ON pending_collection (finance_client_id);
+CREATE INDEX idx_pending_collection_status ON pending_collection (status);
 CREATE SEQUENCE pending_collection_id_seq;
 
 -- +goose Down
+DROP INDEX idx_pending_collection_client_id;
+DROP INDEX idx_pending_collection_status;
 DROP SEQUENCE pending_collection_id_seq;
 DROP TABLE pending_collection;
