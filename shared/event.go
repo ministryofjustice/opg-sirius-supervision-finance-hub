131b7de086feeb77b561a69ddfaba0d0e7dc0fbc
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2025-09-30 15:33:25 +0100

    SFS-56 Handle client made inactive event

--- shared/event.go
+++ shared/event.go
@@ -12,6 +12,7 @@ const (
 	DetailTypeFinanceAdhoc              = "finance-adhoc"
 	DetailTypeDebtPositionChanged       = "debt-position-changed"
 	DetailTypeClientCreated             = "client-created"
+	DetailTypeClientMadeInactive        = "client-made-inactive"
 	DetailTypeFinanceAdminUpload        = "finance-admin-upload"
 	DetailTypeScheduledEvent            = "scheduled-event"
 	ScheduledEventRefundExpiry          = "refund-expiry"
@@ -58,6 +59,12 @@ func (e *Event) UnmarshalJSON(data []byte) error {
 			return err
 		}
 		e.Detail = detail
+	case DetailTypeClientMadeInactive:
+		var detail ClientMadeInactiveEvent
+		if err := json.Unmarshal(raw.Detail, &detail); err != nil {
+			return err
+		}
+		e.Detail = detail
 	case DetailTypeFinanceAdhoc:
 		var detail AdhocEvent
 		if err := json.Unmarshal(raw.Detail, &detail); err != nil {
@@ -84,6 +91,12 @@ type DebtPositionChangedEvent struct {
 type ClientCreatedEvent struct {
 	ClientID int32  `json:"clientId"`
 	CourtRef string `json:"courtRef"`
+}
+
+type ClientMadeInactiveEvent struct {
+	ClientID int32  `json:"clientId"`
+	CourtRef string `json:"courtRef"`
+	Surname  string `json:"surname"`
 }
 
 type FinanceAdminUploadEvent struct {
