d55e53754c90bd69c047a2073d112d0d7dddd8cd
Kate Smith <kate.smith5@justice.gov.uk>
2026-02-25 11:47:21 +0000

    sfs-361 update query to only register af letters

--- finance-api/internal/store/annual_billing_information.sql.go
+++ finance-api/internal/store/annual_billing_information.sql.go
@@ -30,7 +30,10 @@ WHERE i.startdate::DATE >= ($1)::DATE
       AND o.orderstatus = 'ACTIVE'
       AND c.clientstatus <> 'DEATH_NOTIFIED'
     )
-    OR ies.invoice_id IS NOT NULL
+    OR (
+        ies.invoice_id IS NOT NULL
+        AND ies.templateid IN ('AF1', 'AF2', 'AF3')
+    )
 )
 GROUP BY ies.status, fc.payment_method
 `
